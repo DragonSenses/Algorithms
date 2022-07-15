e1f60ae461952999528170437d9d0906acaf7d91
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-15 02:55:57 -0700

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,10 +1,12 @@
+import java.util.Comparator;
+
 /**
  * 
  * Best-  O(n)
  * Avg -  O(n^2)
  * Worst- O(n^2)
  */
-public class InsertionSort {
+public class InsertionSort <E> {
     
     /**
      * Insertion sort on an array of integers 
@@ -41,4 +43,41 @@ public class InsertionSort {
             data[i] = c;                    // 6. Insert element here after loop
         }
     }
+
+    Comparator<E> comp = (E x, E y) -> compare(x,y);
+
+    /**
+     * Compares Two Keys by natural ordering
+     * @param x First key to compare
+     * @param y Second key to compare
+     * @return -1 if x < y, 
+     *          0 if x==y, 
+     *          1 if x > y
+     */
+    protected int compare(E x, E y){
+        return comp.compare(x,y);
+    }
+
+    /**
+     * Insertion sort on an array of generic type 
+     * @param data
+     */
+    public void insertionSort(E[] data){
+        int len = data.length;
+        // 1. Mark First Element as sorted
+        for(int k = 1; k < len; k++){       // Start with Second Character
+            E e = data[k];                  // 2. Extract the element c
+            int i = k;                      // Save the correct index for c
+            while (i > 0 && comp.compare(e,data[i-1]) <= 0){ // 3. If the data[i-1] is greater
+                data[i] = data[i-1];        // 4. Move data[i-1] to the right
+                i--;                        // 5. Repeat until data[i-1] is lesser
+            }
+            data[i] = e;                    // 6. Insert element here after loop
+        }
+    }
+    // data[i-1] > e
+    // e < data[i-1]
+    // x < y
+    // -1 or 0 (if duplicates allowed
+    // 
 }
