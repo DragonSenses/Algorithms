35d54640b11d5413f9abb775a16d6000eb254d9d
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-18 20:40:10 -0700

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,26 @@
+
+/**
+ * The Fibonacci series, is a series of numbers in which each number
+ * (Fibonacci number) is the sum of the two preceding numbers.
+ * 
+ * Fn = {0, 1, 1, 2, 3, 5, 8, 13, 21, 
+ *      34, 55, 89, 144, 233, 377, 610, 987....}
+ * Fn = Fn-1 + Fn-2
+ * 
+ * If one were to draw a line starting in the right bottom corner of
+ * a golden rectangle within the first square and then touch each 
+ * succeeding multiple squares outside corners, you would create a 
+ * Fibonacci spiral.
+ * 
+ * The beauty in this sequence is how it represents structures and 
+ * sequences that model physical reality, as it appears in the 
+ * smallest to the largest objects in Nature. From petals,
+ * rows of seeds, chicken eggs, nautilus shells, and even spirals.
+ * 
+ * The Fibonacci sequence is a way for information to flow in a very 
+ * efficient manner. 
+ * 
+ */
+public class Fibonacci {
+    
+}
