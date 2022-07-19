bd82ebd2d0ec1a270350814ee69e0fa61f1222a3
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-18 20:53:22 -0700

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -20,7 +20,58 @@
  * The Fibonacci sequence is a way for information to flow in a very 
  * efficient manner. 
  * 
+ * We can generate the progression of Fibonacci numbers by defining
+ * recursively 
+ * - F0 = 0
+ * - F1 = 1
+ * - Fn = Fn-2 + Fn-1   (for n > 1)
+ * 
  */
 public class Fibonacci {
     
+    /**
+     * Time Complexity of Recursive Fibonacci
+     * F(0) = 0, F(1) = 1, F(n) = F(n-1) + F(n-2)
+     * 
+     * The recursive equation is
+     * T(n) = T(n-1) + T(n-2) + O(1)
+     * Time to calculate fb(n) is equal to sum of time
+     * to calculate fb(n-1) and fb(n-2) including 
+     * constant time to perform previous additions.
+     * 
+     * Characteristic equation is 
+     * x^2 = x + 1
+     * x^2 - x - 1 = 0
+     * 
+     * Roots: 
+     * x1 = (1+sqrt(5))/2 
+     * x2 = (1-sqrt(5))/2
+     * 
+     * Plug in roots to characteristic equation where linear
+     * recursive function is
+     * F(n) = (x1)^n + (x2)^n
+     * F(n) = [(1+sqrt(5))/2]^n + [(1-sqrt(5))/2]^n
+     * 
+     * T(n) and F(n) are asymptotically the same as both functions
+     * represent the same sequence. Therefore, dropping lower order
+     * terms via Big O notation, we get: 
+     * T(n) = O(([(1+sqrt(5))/2]^n + [(1-sqrt(5))/2]^n))
+     * T(n) = O((1+sqrt(5) /2)^n)
+     * T(n) = O(1.6180)^n
+     */
+
+    /**
+     * Returns the nth Fibonacci Number (inefficiently)
+     * This recursive implementation computes a Fibonacci number
+     * by making two recursive calls in each non-base case. 
+     * @param n
+     * @return
+     */
+    public static long fibonacciBad(int n){
+        if( n <= 1) {
+            return n;
+        } else {
+            return fibonacciBad(n-2) + fibonacciBad(n-1);
+        }
+    }
 }
