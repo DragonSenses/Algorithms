8ad2df4c7f453add06db3ae312c9875b2af6eb9f
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-26 19:02:02 -0700

    Update InsertionSort.java
    Explanation on advantages, usage, best case.

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,8 +1,44 @@
 import java.util.Comparator;
 
 /**
+ * A simple sorting algorithm that builds the final sorted array (or list) one
+ * at a time. Iterates through a collection, consuming one input element each
+ * repetition and grows a sorted output list. At each iteration, it removes 
+ * one element from the input data, finds the location it belongs within the
+ * sorted list, and inserts it there. It repeats until no input elements remain.
+ * Typically sorting is done in-place, by iterating up the array, growing the 
+ * sorted list behind it. At each-array position it checks the value against the
+ * largest value in the sorted list. If larger it leaves the element in place
+ * and moves to the next. If smaller, it finds the correct position within the
+ * sorted list, shifts all the larger values up to make space, and inserts it
+ * into the correct position. 
  * 
- * Best-  O(n)
+ * Less efficient on large lists than more advanced sorts (heapsort, mergesort,
+ * quicksort). HOWEVER, the Straight Insertion Sort is best for small or very
+ * nearly sorted lists. In fact, it is one of the fastest algorithms for 
+ * sorting very small arrays, even faster than quicksort. As good quicksort 
+ * implementations use insertion sort for arrays smaller than a certain threshold,
+ * also when arising as subproblems. Exact threshold depends on machine and must
+ * be determined experimentally, but commonly is around ten.
+ * 
+ * Advantages:
+ * - Simple Implementation
+ * - Efficient for small data sets
+ * - Adaptive: meaning it is efficient for data sets that are already substantially 
+ *             sorted; the time complexity is O(kn) where each element in the input
+ *             is no more than k places away from its sorted position
+ * - Stable: Does not change the relative order of elements with equal keys
+ * - In-Place: Only requires a constant amount O(1) of additional memory space
+ * - Online: Can sort a list as it receives it
+ * 
+ * Runtimes: Best case input of insertion sort is an array that is already sorted, so
+ * it has a linear running time. During each iteration, the first remaining element of
+ * the input is only compared with the right-most element of the sorted subsection of 
+ * the array. Worst case input is an array sorted in reverse order, every iteration of 
+ * the inner loop will scan and shift the entire sorted subsection of the array before 
+ * inserting the next element giving it a quadratic running time. 
+ * 
+ * Best-  O(n)     
  * Avg -  O(n^2)
  * Worst- O(n^2)
  */
