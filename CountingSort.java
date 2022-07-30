f0eaa212ad41d33e70f9123f26b53ef826e479f1
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-29 22:25:39 -0700

    Update CountingSort.java

--- CountingSort.java
+++ CountingSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Counting Sort is a stable, non-comparative sorting algorithm,
  * with it's main use is for sorting arrays of non-negative integers.
@@ -11,14 +13,26 @@
  * which is typically named the count array. It uses the auxilliary 
  * count array to determine the positions of elements
  * 
- * Time Complexity 
  * N is number of elements
  * K is range of elements (largest element - smallest element)
- * 
+ * Time Complexity 
  * Best-  O(n+k)  // When all elements are the same   
  * Avg -  O(n^2)  // N & K are equally dominant
  * Worst- O(n^2)  // When data is skewed and range is large
+ * 
+ * Space Complexity : O(k)
  */
 public class CountingSort {
     
+    // Simple Implementation - Elements are only single digits ranging from 0-9
+    public static int[] SimpleCountingSort(int[] arr){
+
+        return arr;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {0, 8, 4, 7, 9, 1, 1, 7};
+        int[] sortedArr = SimpleCountingSort(arr);
+        System.out.println(Arrays.toString(sortedArr));
+    }
 }
