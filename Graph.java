d047620a140139a2ea8628574b5ff531070ed916
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-31 21:28:52 -0700

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -5,8 +5,8 @@ import java.util.LinkedList;
  * Graph Implementation using an adjacency Map
  */
 public class Graph <E>{
-    // Graph uses node maps to a linkedlist of all its neighbors
-    private HashMap<Node<Integer>, LinkedList<Node<Integer>>> adjacencyMap;
+    // Graph uses node maps to a LinkedList of all its neighbors
+    private HashMap<Node<E>, LinkedList<Node<E>>> adjacencyMap;
     private boolean directed; // Whether Graph is directed or undirected    
 
     public Graph(boolean directed){
@@ -15,12 +15,15 @@ public class Graph <E>{
     }
 
     /**
-     * Checks for possible duplicate edges, before adding an edge
-     * containg the Nodes u -> v, 
+     * Checks for possible duplicate edges, before adding an edge.
+     * containg the Nodes u -> v, then update the list of neighbors
      * @param u The origin node of the edge
      * @param v The destination node of the edge
      */
     private void validateEdge(Node<E> u, Node<E> v) {
+        // Create a temperorary reference to thhe list of neighbors
+        LinkedList<Node<E>> list = adjacencyMap.get(u);
+        // If list is non-null, and Node u already contains its own list of neighbors
 
     }
 
@@ -30,7 +33,23 @@ public class Graph <E>{
      * @param v The destination node of the edge
      */
     public void addEdge(Node<E> u, Node<E> v){
+        // 1. If adjacency map does not contain Node u, then put it in
+        if(!adjacencyMap.keySet().contains(u)) {
+            adjacencyMap.put(u,null);
+        }
 
+        // 2. Likewise, if adjacency map does not contain Node v, then put it in
+        if(!adjacencyMap.keySet().contains(v)){
+            adjacencyMap.put(v,null);
+        }
+
+        // 3. Validate the Edge, adding to their List of neighbors
+        validateEdge(u,v);
+
+        // 4. If Graph is undirected, add the edge in reverse, from destination to origin
+        if(!directed) {
+            validateEdge(v,u);
+        }
     }
 
     /**
