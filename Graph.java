e4d7cb841fe04c907daa0916e0de936f157fed0b
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-31 21:09:03 -0700

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,10 +1,21 @@
-public class Graph {
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Graph <E>{
     // Graph uses node maps to a linkedlist of all its neighbors
-    private HashMap<Node, LinkedList<Node>> adjacencyMap;
+    private HashMap<Node<Integer>, LinkedList<Node<Integer>>> adjacencyMap;
     private boolean directed; // Whether Graph is directed or undirected    
 
     public Graph(boolean directed){
         this.directed = directed;
         adjacencyMap = new HashMap<>();
+    }
+
+    private void validateEdge(Node<E> u, Node<E> v) {
+
+    }
+
+    public void addEdge(Node<E> u, Node<E> v){
+
     }
 }
