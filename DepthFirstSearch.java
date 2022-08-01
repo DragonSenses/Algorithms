7bec112ce22277c08676ac20d1afb150b153228f
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-31 21:46:50 -0700

    Create DepthFirstSearch.java

---
+++ DepthFirstSearch.java
@@ -0,0 +1,15 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+// import java.util.Stack;
+
+public class DepthFirstSearch <E> {
+    
+    /**
+     * A Depth First Search without Recursion
+     * @param root The first node to begin the search
+     */
+    public void DepthFirstSearchIterative(Node<E> root){
+        Deque<Node<E>> stack = new ArrayDeque<>();
+        stack.push(root);
+    }
+}
