e51535c4c1b285c189f61be2aa9dc436328ee44a
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-31 21:52:45 -0700

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,10 +1,18 @@
 public class Node <E> {
-    E data;
-    boolean visited; // Tracks when a node has been visited
+    private E data;
+    public boolean visited; // Tracks when a node has been visited
 
     public Node(E data) {
         this.data = data;
         visited = false;
+    }
+
+    public E getData(){
+        return data;
+    }
+
+    public boolean isVisited(){
+        return visited;
     }
 
     public void visit() {
