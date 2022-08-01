c05772bdce64b8872014a20e85c5970e9175cf71
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-31 21:17:13 -0700

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,6 +1,9 @@
 import java.util.HashMap;
 import java.util.LinkedList;
 
+/**
+ * Graph Implementation using an adjacency Map
+ */
 public class Graph <E>{
     // Graph uses node maps to a linkedlist of all its neighbors
     private HashMap<Node<Integer>, LinkedList<Node<Integer>>> adjacencyMap;
@@ -11,11 +14,31 @@ public class Graph <E>{
         adjacencyMap = new HashMap<>();
     }
 
+    /**
+     * Checks for possible duplicate edges, before adding an edge
+     * containg the Nodes u -> v, 
+     * @param u The origin node of the edge
+     * @param v The destination node of the edge
+     */
     private void validateEdge(Node<E> u, Node<E> v) {
 
     }
 
+    /**
+     * Adds the edge with nodes u & v in the Graph.
+     * @param u The origin node of the edge
+     * @param v The destination node of the edge
+     */
     public void addEdge(Node<E> u, Node<E> v){
 
+    }
+
+    /**
+     * @returns True if the graph contains the edge with nodes u and v,
+     *          false otherwise
+     */
+    public boolean containsEdge(Node<E> u, Node<E> v){
+        return adjacencyMap.containsKey(u) && 
+            adjacencyMap.get(u).contains(v);
     }
 }
