c9ac2c063c6d6f4af5e11afb7c79f780948619c8
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-31 21:52:50 -0700

    Update DepthFirstSearch.java

--- DepthFirstSearch.java
+++ DepthFirstSearch.java
@@ -11,5 +11,15 @@ public class DepthFirstSearch <E> {
     public void DepthFirstSearchIterative(Node<E> root){
         Deque<Node<E>> stack = new ArrayDeque<>();
         stack.push(root);
+
+        Node<E> curr;    // Keep track of current node
+        while(!stack.isEmpty()) { // While Stack is non-empty
+            curr = stack.pop(); 
+            if(!(curr.isVisited())){
+                curr.visit();   // Visit the current node
+                // For every node
+                for(Node<E> node : adjacencyMap.get(curr) )
+            }
+        }
     }
 }
