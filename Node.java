22ab45d65e10d829accb7298a22d4c2ad8d2b53e
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-31 21:04:53 -0700

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,17 @@
+public class Node <E> {
+    E data;
+    boolean visited; // Tracks when a node has been visited
+
+    public Node(E data) {
+        this.data = data;
+        visited = false;
+    }
+
+    public void visit() {
+        visited = true;
+    }
+
+    public void clear(){
+        visited = false;
+    }
+}
