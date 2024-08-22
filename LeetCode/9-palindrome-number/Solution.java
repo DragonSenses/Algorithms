98c4f4a887434bb58198699904a85a8ef74d0b7e
DragonSenses <kendrickfrancisco@gmail.com>
2024-08-21 18:25:52 -0700

    feat: Add 32-bit constraint to palindromic number

--- LeetCode/9-palindrome-number/Solution.java
+++ LeetCode/9-palindrome-number/Solution.java
@@ -10,11 +10,35 @@ class Solution {
       return false;
     }
 
+    // final int MIN_INT32 = Integer.MIN_VALUE; //-2147483648
+    // final int MAX_INT32 = Integer.MAX_VALUE; // 2147483647
+
+    final int MIN_DIVIDED_BY_10 = Integer.MIN_VALUE / 10; // -214748364
+    final int MAX_DIVIDED_BY_10 = Integer.MAX_VALUE / 10; //  214748364
+
+    final int MIN_LAST_DIGIT = Integer.MIN_VALUE % 10; // 8
+    final int MAX_LAST_DIGIT = Integer.MAX_VALUE % 10; // 7
+
     int original = x;
     int reversed = 0;
+    int digit;
 
     while (x != 0) {
-      reversed = reversed * 10 + x % 10; // Reverse the digits
+      digit = x % 10;
+
+      // Constraint: reversing result is greater than maximum signed 32-bit integer
+      if (reversed > MAX_DIVIDED_BY_10 || 
+        (reversed == MAX_DIVIDED_BY_10 && digit >= MAX_LAST_DIGIT)) {
+        return false;
+      }
+      
+      // Constraint: reversing result is less than minimum signed 32-bit integer
+      if (reversed < MIN_DIVIDED_BY_10 || 
+        (reversed == MIN_DIVIDED_BY_10 && digit <= MIN_LAST_DIGIT)) {
+        return false;
+      }
+   
+      reversed = reversed * 10 + digit; // Reverse the digits
       x /= 10; // Truncate the number
     }
 
