6c72da5548aa542cd5dbaa87d2b9dffb9adcd977
DragonSenses <kendrickfrancisco@gmail.com>
2024-09-02 20:57:27 -0700

    feat(#8): Improve atoi parsing with 32-bit limit

--- LeetCode/8-string-to-integer-atoi/Solution.java
+++ LeetCode/8-string-to-integer-atoi/Solution.java
@@ -17,27 +17,44 @@ class Solution {
     int sign = 1;
     int index = 0;
     if (s.charAt(0) == '-') {
-        sign = -1;
-        index++;
+      sign = -1;
+      index++;
     } else if (s.charAt(0) == '+') {
-        index++;
+      index++;
     }
 
-    long result = 0;
+    // Store result in 32-bit signed integer (assumes environment disallows longs, BigInteger, etc.)
+    int result = 0;
+
+    final int MIN_INT32 = -2147483648; //-2147483648
+    final int MAX_INT32 = 2147483647;  // 2147483647
+
+    final int MIN_INT_DIV10 = Integer.MIN_VALUE / 10; // -214748364
+    final int MAX_INT_DIV10 = Integer.MAX_VALUE / 10; //  214748364
     
+    final int MIN_LAST_DIGIT = 8; // -2147483648 % 10
+    final int MAX_LAST_DIGIT = 7; //  2147483647 % 10
+
     // 3. Traverse next digits of input. Halt when non-digit violates conditions
     // Also end conversion when the end of string is reached
-    while (index < s.length() && Character.isDigit(s.charAt(index))) {
+    while (index < s.length() && s.charAt(index) >= '0' && s.charAt(index) <= '9') {
+      // Convert character representing a digit into integer (e.g., '7'-'0' = 55-48 = 7)
       int digit = s.charAt(index) - '0';
-      result = result * 10 + digit;
       
-      // Step 4: Handle overflow and underflow
-      if (sign * result > Integer.MAX_VALUE) {
-        return Integer.MAX_VALUE;
-      } else if (sign * result < Integer.MIN_VALUE) {
-        return Integer.MIN_VALUE;
+      // Step 4: Check for overflow before updating result
+      if (result > MAX_INT_DIV10) {
+        return (sign == 1) ? MAX_INT32 : MIN_INT32;
       }
 
+      if (result == MAX_INT_DIV10) {
+        if (digit == 8) {
+          return (sign == 1) ? MAX_INT32 : MIN_INT32;
+        } else if (digit > 8) {
+          return (sign == 1) ? MAX_INT32 : MIN_INT32;
+        }
+      }
+
+      result = result * 10 + digit;
       index++;
     }
 
