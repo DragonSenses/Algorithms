544a06d15532c21cf3d9f214191fcb4c8b34465e
DragonSenses <kendrickfrancisco@gmail.com>
2024-09-12 21:34:30 -0700

    feat(#121): Implement single-pass solution in Java

--- LeetCode/121-best-time-to-buy-and-sell-stock/Solution.java
+++ LeetCode/121-best-time-to-buy-and-sell-stock/Solution.java
@@ -1,5 +1,22 @@
 class Solution {
+  /**
+   * Calculates the maximum profit that can be achieved from a single buy and sell transaction.
+   *
+   * @param prices An array where prices[i] is the price of a given stock on the ith day.
+   * @return The maximum profit that can be achieved. If no profit is possible, returns 0.
+   */
   public int maxProfit(int[] prices) {
+    int minPrice = Integer.MAX_VALUE;
+    int maxProfit = 0;
 
+    for (int i = 0; i < prices.length; i++){
+      if (prices[i] < minPrice) {
+        minPrice = prices[i];
+      } else if (prices[i] - minPrice > maxProfit) {
+        maxProfit = prices[i] - minPrice;
+      }
+    }
+
+    return maxProfit;
   }
 }
