c86f1b02f64ec948912dcc03bb86f7375db6ba9d
DragonSenses <kendrickfrancisco@gmail.com>
2024-09-13 20:51:17 -0700

    feat(#121): Add typescript one-pass solution

--- LeetCode/121-best-time-to-buy-and-sell-stock/solution.ts
+++ LeetCode/121-best-time-to-buy-and-sell-stock/solution.ts
@@ -1,3 +1,20 @@
+/**
+ * Calculates the maximum profit that can be achieved from a single buy and sell transaction.
+ *
+ * @param prices An array where prices[i] is the price of a given stock on the ith day.
+ * @return The maximum profit that can be achieved. If no profit is possible, returns 0.
+ */
 function maxProfit(prices: number[]): number {
-    
+  let minPrice = Number.MAX_VALUE;
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    } else if (prices[i] - minPrice > maxProfit) {
+      maxProfit = prices[i] - minPrice;
+    }
+  }
+
+  return maxProfit;
 };
