4a3bc13f9e96d701b0c638130650889fe5e77431
DragonSenses <kendrickfrancisco@gmail.com>
2024-09-16 18:36:30 -0700

    feat(#19): Add two-pass nth node removal

--- LeetCode/19-remove-nth-node-from-end-of-list/Solution.java
+++ LeetCode/19-remove-nth-node-from-end-of-list/Solution.java
@@ -1,15 +1,37 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
+  /**
+   * Removes the n-th node from the end of the list and returns the head of the modified list.
+   *
+   * @param head The head of the singly-linked list.
+   * @param n The position from the end of the list of the node to be removed.
+   * @return The head of the modified list.
+   */
   public ListNode removeNthFromEnd(ListNode head, int n) {
-      
+    ListNode sentinel = new ListNode(0);
+    sentinel.next = head;
+
+    ListNode curr = head;
+    int len = 0;
+
+    // First pass to find the length of the list
+    for (; curr != null; curr = curr.next) {
+      len++;
+    }
+
+    // Calculate the position to remove
+    len-=n;
+
+    // Reinitialize curr to the sentinel node
+    curr = sentinel;
+
+    // Move to the (len)th node
+    for (; len > 0; len--) {
+      curr = curr.next;
+    }
+
+    // Remove the nth node from the end
+    curr.next = curr.next.next;
+
+    return sentinel.next;
   }
 }
