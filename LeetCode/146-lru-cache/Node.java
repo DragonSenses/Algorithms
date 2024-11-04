17c282f46856fbdb065fd165a026285eef576433
DragonSenses <kendrickfrancisco@gmail.com>
2024-11-04 14:14:06 -0800

    docs(#146): Add comments to Node class

--- LeetCode/146-lru-cache/Node.java
+++ LeetCode/146-lru-cache/Node.java
@@ -1,8 +1,8 @@
 public class Node {
-  int key;
-  int val;
-  Node next;
-  Node prev;
+  int key;   // Key for the cache entry
+  int val;   // Value for the cache entry
+  Node next; // Pointer to the next node in the linked list
+  Node prev; // Pointer to the previous node in the linked list
 
   public Node(int key, int val) {
     this.key = key;
