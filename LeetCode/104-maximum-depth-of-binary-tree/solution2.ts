d5556141f6e85f88c1d59ed0a2b7f05f72e907c3
DragonSenses <kendrickfrancisco@gmail.com>
2024-11-22 10:54:13 -0800

    feat(#104): Find max depth of binary tree

---
+++ LeetCode/104-maximum-depth-of-binary-tree/solution2.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+/**
+ * Finds the maximum depth of a binary tree using the recursive approach.
+ *
+ * @param root The root node of the binary tree.
+ * @returns The maximum depth of the binary tree.
+ */
+function maxDepth(root: TreeNode | null): number {
+  if (root === null) {
+    return 0;
+  }
+
+  const leftDepth = maxDepth(root.left);
+  const rightDepth = maxDepth(root.right);
+
+  return Math.max(leftDepth, rightDepth) + 1;
+};
