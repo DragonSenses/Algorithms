4b75c71c9f0812f8de43e98f26f105c679b105fe
DragonSenses <kendrickfrancisco@gmail.com>
2024-11-24 18:00:36 -0800

    feat(#100): Queue-based check for tree equality

--- LeetCode/100-same-tree/Solution.java
+++ LeetCode/100-same-tree/Solution.java
@@ -1,3 +1,5 @@
+import java.util.ArrayDeque;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -14,7 +16,35 @@
  * }
  */
 class Solution {
+
+  public boolean isEqual(TreeNode p, TreeNode q) {
+    if (p == null && q == null) return true;
+    if (q == null || p == null) return false;
+    if (p.val != q.val) return false;
+    return true;
+  }
+
   public boolean isSameTree(TreeNode p, TreeNode q) {
-      
+    if (p == null && q == null) return true;
+    if (!isEqual(p, q)) return false;
+
+    ArrayDeque<TreeNode> queueP = new ArrayDeque<>();
+    ArrayDeque<TreeNode> queueQ = new ArrayDeque<>();
+    queueP.addLast(p);
+    queueQ.addLast(q);
+
+    while (!queueP.isEmpty()) {
+      p = queueP.removeFirst();
+      q = queueQ.removeFirst();
+
+      if (!isEqual(p, q)) return false;
+      if (p != null) {
+        queueP.addLast(p.left);
+        queueQ.addLast(q.left);
+        queueP.addLast(p.right);
+        queueQ.addLast(q.right);
+      }
+    }
+    return true;
   }
 }
