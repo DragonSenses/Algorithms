e6cd3a16ba13d4e19eb30e0e891e47f1113296a7
DragonSenses <kendrickfrancisco@gmail.com>
2024-12-01 10:45:21 -0800

    docs(#94): Add comments for clarity

--- LeetCode/94-binary-tree-inorder-traversal/Solution3.java
+++ LeetCode/94-binary-tree-inorder-traversal/Solution3.java
@@ -1,25 +1,44 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class provides a solution to perform Morris Traversal on a binary tree.
+ * Morris Traversal is an in-order traversal algorithm with O(1) space
+ * complexity.
+ */
 public class Solution3 {
+
+  /**
+   * Performs Morris Traversal on the binary tree and returns the in-order
+   * traversal as a list of integers.
+   *
+   * @param root the root node of the binary tree
+   * @return a list of integers representing the in-order traversal of the binary
+   *         tree
+   */
   public List<Integer> inorderTraversal(TreeNode root) {
     List<Integer> result = new ArrayList<>();
     TreeNode current = root;
 
+    // Traverse the tree
     while (current != null) {
       if (current.left == null) {
+        // If there is no left child, visit this node and move to the right
         result.add(current.val);
         current = current.right;
       } else {
+        // Find the inorder predecessor of current
         TreeNode predecessor = current.left;
         while (predecessor.right != null && predecessor.right != current) {
           predecessor = predecessor.right;
         }
 
         if (predecessor.right == null) {
+          // Establish the link to return to the current node
           predecessor.right = current;
           current = current.left;
         } else {
+          // Remove the established link and visit this node
           predecessor.right = null;
           result.add(current.val);
           current = current.right;
