7b407c8dea32697212b1571ed1a7a0fde0c4ee7b
DragonSenses <kendrickfrancisco@gmail.com>
2024-12-02 19:41:11 -0800

    docs(#93): Add comments for clarity

--- LeetCode/93-restore-ip-addresses/Solution.java
+++ LeetCode/93-restore-ip-addresses/Solution.java
@@ -1,25 +1,50 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class provides a method to restore valid IP addresses from a given
+ * string.
+ */
 public class Solution {
+
+  /**
+   * Restores valid IP addresses from the given string.
+   *
+   * @param s the input string containing only digits
+   * @return a list of all possible valid IP address combinations
+   */
   public static List<String> restoreIpAddresses(String s) {
     List<String> ans = new ArrayList<>();
     backtrack(s, 0, new ArrayList<>(), ans);
     return ans;
   }
 
+  /**
+   * Utility method to backtrack and find all valid IP address combinations.
+   *
+   * @param s          the input string containing only digits
+   * @param startIndex the current index to start processing
+   * @param dots       a list of integers representing the positions of the dots
+   *                   added so far
+   * @param ans        a list of strings to store the valid IP address
+   *                   combinations
+   */
   private static void backtrack(String s, int startIndex, List<Integer> dots, List<String> ans) {
     int remainingLength = s.length() - startIndex;
     int remainingNumberOfIntegers = 4 - dots.size();
 
+    // Check if the remaining length is too long or too short to form a valid IP
+    // address
     if (remainingLength > remainingNumberOfIntegers * 3 || remainingLength < remainingNumberOfIntegers) {
       return;
     }
 
+    // If we are adding the last segment
     if (remainingNumberOfIntegers == 1) {
       if (validSegment(s, startIndex, remainingLength)) {
         StringBuilder ipAddress = new StringBuilder();
         int last = 0;
+        // Build the IP address with dots
         for (int dot : dots) {
           ipAddress.append(s, last, last + dot).append('.');
           last += dot;
@@ -30,6 +55,7 @@ public class Solution {
       return;
     }
 
+    // Try to place a dot after every 1 to 3 digits
     for (int curPos = 1; curPos <= Math.min(3, remainingLength); curPos++) {
       if (validSegment(s, startIndex, curPos)) {
         dots.add(curPos);
@@ -39,6 +65,14 @@ public class Solution {
     }
   }
 
+  /**
+   * Checks if the segment of the string is a valid IP address segment.
+   *
+   * @param s      the input string
+   * @param start  the starting index of the segment
+   * @param length the length of the segment
+   * @return true if the segment is valid, false otherwise
+   */
   private static boolean validSegment(String s, int start, int length) {
     if (length == 1) {
       return true;
