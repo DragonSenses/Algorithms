c74040210825c8cc40ed1bc0208bd34ff6627079
DragonSenses <kendrickfrancisco@gmail.com>
2024-12-13 18:38:12 -0800

    feat(#70): Implement stair climb with DP approach

---
+++ LeetCode/70-climbing-stairs/solution3.ts
@@ -0,0 +1,14 @@
+function climbStairs(n: number): number {
+  if (n === 1) {
+    return 1;
+  }
+
+  const dp: number[] = new Array(n + 1);
+  dp[1] = 1;
+  dp[2] = 2; 
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
+}
