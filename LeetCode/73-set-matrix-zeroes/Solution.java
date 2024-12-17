3bad9af709a9e9d7356480d182428e0541fbf057
DragonSenses <kendrickfrancisco@gmail.com>
2024-12-16 18:34:43 -0800

    feat(#73): Implement two-pass matrix zeroing

--- LeetCode/73-set-matrix-zeroes/Solution.java
+++ LeetCode/73-set-matrix-zeroes/Solution.java
@@ -1,5 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
   public void setZeroes(int[][] matrix) {
-      
+    int R = matrix.length;
+    int C = matrix[0].length;
+    Set<Integer> rows = new HashSet<>();
+    Set<Integer> cols = new HashSet<>();
+
+    // First pass to find all rows and columns that contain zero
+    for (int i = 0; i < R; i++) {
+      for (int j = 0; j < C; j++) {
+        if (matrix[i][j] == 0) {
+          rows.add(i);
+          cols.add(j);
+        }
+      }
+    }
+
+    // Second pass to set the cells to zero
+    for (int i = 0; i < R; i++) {
+      for (int j = 0; j < C; j++) {
+        if (rows.contains(i) || cols.contains(j)) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
   }
 }
