507b4bec0b3d91ed5ccf9cb035c25dd67806d79a
DragonSenses <kendrickfrancisco@gmail.com>
2024-12-17 19:02:44 -0800

    feat(#73): Set zeroes in matrix with 2 passes

--- LeetCode/73-set-matrix-zeroes/solution.ts
+++ LeetCode/73-set-matrix-zeroes/solution.ts
@@ -1,6 +1,25 @@
-/**
- Do not return anything, modify matrix in-place instead.
- */
- function setZeroes(matrix: number[][]): void {
-    
- };
+function setZeroes(matrix: number[][]): void {
+  const rows = new Set<number>();
+  const cols = new Set<number>();
+  const R = matrix.length;
+  const C = matrix[0].length;
+
+  // First pass to find zeros and mark rows and columns
+  for (let i = 0; i < R; i++) {
+    for (let j = 0; j < C; j++) {
+      if (matrix[i][j] === 0) {
+        rows.add(i);
+        cols.add(j);
+      }
+    }
+  }
+
+  // Second pass to set matrix cells to zero
+  for (let i = 0; i < R; i++) {
+    for (let j = 0; j < C; j++) {
+      if (rows.has(i) || cols.has(j)) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+}
