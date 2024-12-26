b8117456fac012699bae87f69715de02cc912bc4
DragonSenses <kendrickfrancisco@gmail.com>
2024-12-25 17:16:42 -0800

    feat(#66): Add carry-based addition to increment

--- LeetCode/66-plus-one/solution.ts
+++ LeetCode/66-plus-one/solution.ts
@@ -1,3 +1,17 @@
 function plusOne(digits: number[]): number[] {
-    
+  // Traverse the array from the end (least significant digit) to the start (most significant digit)
+  for (let i = digits.length - 1; i >= 0; i--) {
+    // If the current digit is not 9, increment it by one and return the result
+    if (digits[i] !== 9) {
+      digits[i]++;
+      return digits;
+    }
+    // If the current digit is 9, set it to 0 and continue the loop
+    digits[i] = 0;
+  }
+
+  // If all digits were 9, we need to add an extra 1 at the beginning of the array
+  digits.unshift(1);
+
+  return digits;
 };
