43f9c2b16aaee847d2d6431349f44d1bee131af4
DragonSenses <kendrickfrancisco@gmail.com>
2024-12-26 19:41:08 -0800

    feat(#66): Increment digits with carry addition

--- LeetCode/66-plus-one/Solution.java
+++ LeetCode/66-plus-one/Solution.java
@@ -1,5 +1,27 @@
 class Solution {
+  /**
+   * Increment the given array of digits representing a large integer by one.
+   *
+   * @param digits The array of digits representing the large integer.
+   * @return The array of digits after incrementing by one.
+   */
   public int[] plusOne(int[] digits) {
-      
+    // Traverse the array from the end (least significant digit) to the start (most significant
+    // digit)
+    for (int i = digits.length - 1; i >= 0; i--) {
+      // If the current digit is not 9, increment it by one and return the result
+      if (digits[i] != 9) {
+        digits[i]++;
+        return digits;
+      }
+      // If the current digit is 9, set it to 0 and continue the loop
+      digits[i] = 0;
+    }
+
+    // If all digits were 9, we need to add an extra 1 at the beginning of the array
+    int[] result = new int[digits.length + 1];
+    result[0] = 1; // The rest of the array is already initialized to 0s
+
+    return result;
   }
 }
