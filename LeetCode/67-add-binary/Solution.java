08c2c367057b132b29be728ce16be8ad3b9467f9
DragonSenses <kendrickfrancisco@gmail.com>
2024-12-27 15:38:44 -0800

    feat(#67): Compute binary sum with toBinaryString

--- LeetCode/67-add-binary/Solution.java
+++ LeetCode/67-add-binary/Solution.java
@@ -1,5 +1,12 @@
 class Solution {
+  /**
+   * Adds two binary strings and returns their sum as a binary string.
+   *
+   * @param a The first binary string.
+   * @param b The second binary string.
+   * @return The sum of the two binary strings as a binary string.
+   */
   public String addBinary(String a, String b) {
-      
+    return Integer.toBinaryString(Integer.parseInt(a, 2) + Integer.parseInt(b, 2));
   }
 }
