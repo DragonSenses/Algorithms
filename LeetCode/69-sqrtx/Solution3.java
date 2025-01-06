09a07dbef5168a9783b3f94f8f3d490f86903379
DragonSenses <kendrickfrancisco@gmail.com>
2025-01-05 20:14:23 -0800

    feat(#69): Add comments and Javadoc for clarity

--- LeetCode/69-sqrtx/Solution3.java
+++ LeetCode/69-sqrtx/Solution3.java
@@ -1,17 +1,39 @@
-class Solution3 {
+/**
+ * Solution class to find the integer part of the square root of a number using binary search.
+ */
+public class Solution3 {
+
+  /**
+   * Finds the integer part of the square root of x.
+   *
+   * @param x The number for which to find the square root.
+   * @return The integer part of the square root of x.
+   */
   public int mySqrt(int x) {
-    if (x < 2) return x;
+    // If x is less than 2, return x itself as the square root.
+    if (x < 2)
+      return x;
 
     long num;
     int pivot, left = 2, right = x / 2;
+
+    // Perform binary search to find the square root.
     while (left <= right) {
       pivot = left + (right - left) / 2;
       num = (long) pivot * pivot;
-      if (num > x) right = pivot - 1;
-      else if (num < x) left = pivot + 1;
-      else return pivot;
+
+      // If num is greater than x, move the right pointer.
+      if (num > x)
+        right = pivot - 1;
+      // If num is less than x, move the left pointer.
+      else if (num < x)
+        left = pivot + 1;
+      // If num is equal to x, return pivot as the square root.
+      else
+        return pivot;
     }
 
+    // Return right as the integer part of the square root.
     return right;
   }
 }
