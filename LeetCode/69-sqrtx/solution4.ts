1a720cb2a9e6ec084d92a05544e1441bb28e3f88
DragonSenses <kendrickfrancisco@gmail.com>
2025-01-05 20:27:59 -0800

    docs(#69): Add comments before implementation

---
+++ LeetCode/69-sqrtx/solution4.ts
@@ -0,0 +1,13 @@
+/**
+ * Finds the integer part of the square root of a given number using Newton's method.
+ *
+ * @param x - The number for which to find the square root.
+ * @return The integer part of the square root of x.
+ */
+function mySqrt(x: number): number {
+  // Base case: If x is less than 2, return x itself as the square root.
+  // Initialize x0 with x.
+  // Compute the next estimate x1 using the formula.
+  // Iterate until the difference between x0 and x1 is less than 1.
+  // Return the integer part of x1.
+}
