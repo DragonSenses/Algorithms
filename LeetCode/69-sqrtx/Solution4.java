b3d7570edd147ac925fe74cb3da6a31c4abef860
DragonSenses <kendrickfrancisco@gmail.com>
2025-01-06 17:56:45 -0800

    feat(#69): Compute sqrt using Newton's method

--- LeetCode/69-sqrtx/Solution4.java
+++ LeetCode/69-sqrtx/Solution4.java
@@ -11,9 +11,19 @@ class Solution4 {
    */
   public int mySqrt(int x) {
     // Base case: if x is less than 2, return x
+    if (x < 2)
+      return x;
+
     // Initialize x0 with x
+    double x0 = x;
     // Compute the next estimate x1 using the formula
+    double x1 = (x0 + x / x0) / 2.0;
     // Iterate until the difference between x0 and x1 is less than 1
+    while (Math.abs(x0 - x1) >= 1) {
+      x0 = x1;
+      x1 = (x0 + x / x0) / 2.0;
+    }
     // Return the integer part of x1
+    return (int) x1;
   }
 }
