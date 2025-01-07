5477011b92eb86ad919f5d4b653d9d7df3573d4f
DragonSenses <kendrickfrancisco@gmail.com>
2025-01-06 17:53:37 -0800

    docs(#69): Add comments before implementation

---
+++ LeetCode/69-sqrtx/Solution4.java
@@ -0,0 +1,19 @@
+/**
+ * Class Solution provides a method to compute the integer square root of a given number using
+ * Newton's Method.
+ */
+class Solution4 {
+  /**
+   * Computes the integer square root of a given number.
+   * 
+   * @param x The number to compute the square root of.
+   * @return The integer square root of the given number.
+   */
+  public int mySqrt(int x) {
+    // Base case: if x is less than 2, return x
+    // Initialize x0 with x
+    // Compute the next estimate x1 using the formula
+    // Iterate until the difference between x0 and x1 is less than 1
+    // Return the integer part of x1
+  }
+}
