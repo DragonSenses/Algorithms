45575701405ed38238b7d28ebf039d9867f3a0e1
DragonSenses <kendrickfrancisco@gmail.com>
2025-01-09 18:08:19 -0800

    docs(#55): Add comments and Javadoc for clarity

--- LeetCode/55-jump-game/Solution3.java
+++ LeetCode/55-jump-game/Solution3.java
@@ -1,13 +1,30 @@
+/**
+ * Enum representing the status of a position in the nums array.
+ */
 enum Index {
   VALID, INVALID, UNKNOWN
 }
 
+
+/**
+ * Solution class for determining if you can reach the last position starting from the first
+ * position in the given nums array.
+ */
 public class Solution3 {
 
+  /**
+   * Determines whether you can reach the last position starting from the first position in the
+   * given nums array.
+   * 
+   * @param nums - The array of non-negative integers where each element represents the maximum jump
+   *        length at that position.
+   * @return true if you can reach the last position from the first position, otherwise false.
+   */
   public boolean canJump(int[] nums) {
+    // Array to store the status of each position
     Index[] memo = new Index[nums.length];
 
-    // Initialize memo array
+    // Initialize memo array with UNKNOWN status
     for (int i = 0; i < memo.length; i++) {
       memo[i] = Index.UNKNOWN;
     }
@@ -15,7 +32,7 @@ public class Solution3 {
     // The last position is always VALID
     memo[memo.length - 1] = Index.VALID;
 
-    // Bottom-up processing
+    // Bottom-up processing to determine the status of each position
     for (int i = nums.length - 2; i >= 0; i--) {
       int furthestJump = Math.min(i + nums[i], nums.length - 1);
       for (int j = i + 1; j <= furthestJump; j++) {
@@ -24,6 +41,7 @@ public class Solution3 {
           break;
         }
       }
+      // If no valid jump is found, mark the position as INVALID
       if (memo[i] != Index.VALID) {
         memo[i] = Index.INVALID;
       }
