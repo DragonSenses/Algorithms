d09bb954d4fa72b9cd9f92b89d523a9c34cd110c
DragonSenses <kendrickfrancisco@gmail.com>
2025-01-12 18:36:37 -0800

    feat(#54): Perform spiral traversal w/ boundaries

--- LeetCode/54-spiral-matrix/Solution.java
+++ LeetCode/54-spiral-matrix/Solution.java
@@ -1,5 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    public List<Integer> spiralOrder(int[][] matrix) {
-        
+  public List<Integer> spiralOrder(int[][] matrix) {
+    List<Integer> result = new ArrayList<>();
+    int rows = matrix.length;
+    int columns = matrix[0].length;
+    int up = 0;
+    int left = 0;
+    int right = columns - 1;
+    int down = rows - 1;
+
+    while (result.size() < rows * columns) {
+      for (int col = left; col <= right; col++) {
+        result.add(matrix[up][col]);
+      }
+      up++;
+
+      for (int row = up; row <= down; row++) {
+        result.add(matrix[row][right]);
+      }
+      right--; 
+
+      if (up <= down) {
+        for (int col = right; col >= left; col--) {
+          result.add(matrix[down][col]);
+        }
+        down--;
+      }
+
+      if (left <= right) {
+        for (int row = down; row >= up; row--) {
+          result.add(matrix[row][left]);
+        }
+        left++;
+      }
     }
+
+    return result;
+  }
 }
