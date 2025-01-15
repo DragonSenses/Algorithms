27822817492f3f852e0e1ac5c3fff652da4a544c
DragonSenses <kendrickfrancisco@gmail.com>
2025-01-14 18:47:43 -0800

    feat(#45): Find minimum num of jumps to reach end

--- LeetCode/45-jump-game-ii/solution.ts
+++ LeetCode/45-jump-game-ii/solution.ts
@@ -1,3 +1,20 @@
 function jump(nums: number[]): number {
-    
-};
+  let curEnd = 0;
+  let curFar = 0;
+  let jumps = 0;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    curFar = Math.max(curFar, i + nums[i]);
+
+    if (i === curEnd) {
+      jumps++;
+      curEnd = curFar;
+
+      if (curEnd >= nums.length - 1) {
+        break;
+      }
+    }
+  }
+
+  return jumps;
+}
