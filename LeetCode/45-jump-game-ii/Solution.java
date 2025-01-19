f88639e710f12fa05daa5d15a3eb6d5c6f5ded22
DragonSenses <kendrickfrancisco@gmail.com>
2025-01-18 17:23:02 -0800

    docs(#45): Add comments and Javadocs

--- LeetCode/45-jump-game-ii/Solution.java
+++ LeetCode/45-jump-game-ii/Solution.java
@@ -1,5 +1,12 @@
-class Solution {
+/**
+ * Greedy algorithm to determine the minimum number of jumps
+ * needed to reach the end of the array.
+ *
+ * @param nums Array of non-negative integers where each element
+ *             represents the maximum jump length from that position.
+ * @return The minimum number of jumps required to reach the end.
+ */
+public class Solution {
   public int jump(int[] nums) {
-      
   }
 }
