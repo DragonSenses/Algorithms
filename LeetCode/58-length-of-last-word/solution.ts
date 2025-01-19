3285496b3a74a9dc1a7dd94bd15fef61ad7e9825
DragonSenses <kendrickfrancisco@gmail.com>
2025-01-18 17:57:07 -0800

    feat(#58): Calculate length of last word in string

--- LeetCode/58-length-of-last-word/solution.ts
+++ LeetCode/58-length-of-last-word/solution.ts
@@ -1,3 +1,20 @@
 function lengthOfLastWord(s: string): number {
-    
-};
+  // Initialize the pointer to the end of the string
+  let i = s.length - 1;
+
+  // Skip trailing spaces
+  while (i >= 0 && s.charAt(i) === " ") {
+    i--;
+  }
+
+  // Initialize the length counter
+  let length = 0;
+
+  // Count the length of the last word
+  while (i >= 0 && s.charAt(i) !== " ") {
+    length++;
+    i--;
+  }
+
+  return length;
+}
