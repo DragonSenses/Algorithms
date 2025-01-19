818bca91cf523dc1b6e90a7845de7631c918a534
DragonSenses <kendrickfrancisco@gmail.com>
2025-01-18 17:23:44 -0800

    feat(#45): Find min. num of jumps via greedy algo

--- LeetCode/45-jump-game-ii/Solution.java
+++ LeetCode/45-jump-game-ii/Solution.java
@@ -8,5 +8,37 @@
  */
 public class Solution {
   public int jump(int[] nums) {
+    if (nums.length <= 1) {
+      return 0;
+    }
+
+    // End of the current jump range
+    int curEnd = 0;
+
+    // Farthest index reachable from the current range
+    int curFar = 0;
+
+    // Number of jumps
+    int jumps = 0;
+
+    for (int i = 0; i < nums.length - 1; i++) {
+      // Update the farthest reach
+      curFar = Math.max(curFar, i + nums[i]);
+
+      if (i == curEnd) {
+        // End of the current jump range
+        jumps++;
+
+        // Move to the next jump range
+        curEnd = curFar;
+
+        // If we can reach or exceed the last index, break
+        if (curEnd >= nums.length - 1) {
+          break;
+        }
+      }
+    }
+
+    return jumps;
   }
 }
