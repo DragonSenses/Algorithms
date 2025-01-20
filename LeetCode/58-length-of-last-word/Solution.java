6b6149f38c86ee9ce13c73b4779b0798b88810a8
DragonSenses <kendrickfrancisco@gmail.com>
2025-01-19 19:02:29 -0800

    docs(#58): Add comments and Javadoc for clarity

--- LeetCode/58-length-of-last-word/Solution.java
+++ LeetCode/58-length-of-last-word/Solution.java
@@ -1,5 +1,14 @@
 public class Solution {
 
+  /**
+   * Finds the length of the last word in the given string.
+   * 
+   * The method iterates through the string from the end to the start, skipping 
+   * any trailing spaces, and then counts the characters of the last word.
+   * 
+   * @param s The input string containing words and spaces.
+   * @return The length of the last word in the string.
+   */
   public static int lengthOfLastWord(String s) {
     // Initialize the pointer to the end of the string
     int i = s.length() - 1;
