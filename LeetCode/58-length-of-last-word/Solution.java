a58fb04265bd2988eb8b82e7be350dd963bdd82d
DragonSenses <kendrickfrancisco@gmail.com>
2025-01-19 18:58:58 -0800

    feat(#58): Compute length of last word
    Implement a function to compute the length of the last word in a given string using string index manipulation.

--- LeetCode/58-length-of-last-word/Solution.java
+++ LeetCode/58-length-of-last-word/Solution.java
@@ -1,5 +1,23 @@
-class Solution {
-  public int lengthOfLastWord(String s) {
-      
+public class Solution {
+
+  public static int lengthOfLastWord(String s) {
+    // Initialize the pointer to the end of the string
+    int i = s.length() - 1;
+
+    // Skip trailing spaces
+    while (i >= 0 && s.charAt(i) == ' ') {
+      i--;
+    }
+
+    // Initialize the length counter
+    int length = 0;
+
+    // Count the length of the last word
+    while (i >= 0 && s.charAt(i) != ' ') {
+      length++;
+      i--;
+    }
+
+    return length;
   }
 }
