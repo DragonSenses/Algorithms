5c664fad0025017fb65439cc4544a4caf48e6252
DragonSenses <kendrickfrancisco@gmail.com>
2025-02-08 10:26:06 -0800

    feat(#59): Generate matrix using spiral traversal

--- LeetCode/59-spiral-matrix-ii/solution.ts
+++ LeetCode/59-spiral-matrix-ii/solution.ts
@@ -1,3 +1,30 @@
 function generateMatrix(n: number): number[][] {
-    
-};
+  const matrix: number[][] = Array.from({ length: n }, () => Array(n).fill(0));
+  
+  let num = 1; 
+  let top = 0, bottom = n - 1, left = 0, right = n - 1; 
+
+  while (num <= n * n) {
+    for (let col = left; col <= right; col++) {
+      matrix[top][col] = num++;
+    }
+    top++; 
+
+    for (let row = top; row <= bottom; row++) {
+      matrix[row][right] = num++;
+    }
+    right--;
+
+    for (let col = right; col >= left; col--) {
+      matrix[bottom][col] = num++;
+    }
+    bottom--;
+
+    for (let row = bottom; row >= top; row--) {
+      matrix[row][left] = num++;
+    }
+    left++; 
+  }
+
+  return matrix;
+}
