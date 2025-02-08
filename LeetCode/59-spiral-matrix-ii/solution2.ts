717cbc6987635e741fd04b208ced3cf0174b4eee
DragonSenses <kendrickfrancisco@gmail.com>
2025-02-08 10:49:23 -0800

    feat(#59): Optimize w/ predefined direction array

--- LeetCode/59-spiral-matrix-ii/solution2.ts
+++ LeetCode/59-spiral-matrix-ii/solution2.ts
@@ -1,35 +1,35 @@
 function generateMatrix(n: number): number[][] {
-  // Initialize a n x n matrix filled with 0s
   const matrix: number[][] = Array.from({ length: n }, () => Array(n).fill(0));
-  
-  let num = 1; // Starting number to fill the matrix
-  let top = 0, bottom = n - 1, left = 0, right = n - 1; // Boundaries of the current layer
+  let num = 1;
+  let row = 0,
+    col = 0;
+  const dir = [
+    [0, 1], // Right
+    [1, 0], // Down
+    [0, -1], // Left
+    [-1, 0], // Up
+  ];
+  let d = 0; // Initial direction is 'right'
 
-  // Fill the matrix layer by layer
   while (num <= n * n) {
-    // Traverse from left to right
-    for (let col = left; col <= right; col++) {
-      matrix[top][col] = num++;
-    }
-    top++; // Move the top boundary down
+    matrix[row][col] = num++;
+    let nextRow = row + dir[d][0];
+    let nextCol = col + dir[d][1];
 
-    // Traverse from top to bottom
-    for (let row = top; row <= bottom; row++) {
-      matrix[row][right] = num++;
+    if (
+      nextRow < 0 ||
+      nextRow >= n ||
+      nextCol < 0 ||
+      nextCol >= n ||
+      matrix[nextRow][nextCol] !== 0
+    ) {
+      d = (d + 1) % 4; // Change direction
+      nextRow = row + dir[d][0];
+      nextCol = col + dir[d][1];
     }
-    right--; // Move the right boundary left
 
-    // Traverse from right to left
-    for (let col = right; col >= left; col--) {
-      matrix[bottom][col] = num++;
-    }
-    bottom--; // Move the bottom boundary up
-
-    // Traverse from bottom to top
-    for (let row = bottom; row >= top; row--) {
-      matrix[row][left] = num++;
-    }
-    left++; // Move the left boundary right
+    row = nextRow;
+    col = nextCol;
   }
 
   return matrix;
