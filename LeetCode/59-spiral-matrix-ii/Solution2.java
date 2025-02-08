fe765ed0fa4e19354fe462bf80ecaba483a00592
DragonSenses <kendrickfrancisco@gmail.com>
2025-02-08 10:40:22 -0800

    feat(#59): Implement initial spiral traversal

---
+++ LeetCode/59-spiral-matrix-ii/Solution2.java
@@ -0,0 +1,40 @@
+class Solution2 {
+
+  /**
+   * Generates a matrix filled with numbers from 1 to n^2 in a spiral order.
+   */
+  public int[][] generateMatrix(int n) {
+    int[][] matrix = new int[n][n];
+    int num = 1; // Starting number to fill the matrix
+    int top = 0, bottom = n - 1, left = 0, right = n - 1; // Boundaries of the current layer
+
+    // Fill the matrix layer by layer
+    while (num <= n * n) {
+      // Traverse from left to right
+      for (int col = left; col <= right; col++) {
+        matrix[top][col] = num++;
+      }
+      top++; // Move the top boundary down
+
+      // Traverse from top to bottom
+      for (int row = top; row <= bottom; row++) {
+        matrix[row][right] = num++;
+      }
+      right--; // Move the right boundary left
+
+      // Traverse from right to left
+      for (int col = right; col >= left; col--) {
+        matrix[bottom][col] = num++;
+      }
+      bottom--; // Move the bottom boundary up
+
+      // Traverse from bottom to top
+      for (int row = bottom; row >= top; row--) {
+        matrix[row][left] = num++;
+      }
+      left++; // Move the left boundary right
+    }
+
+    return matrix;
+  }
+}
