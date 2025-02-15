dff440118ec1ace935807b97149946f70780ffc8
DragonSenses <kendrickfrancisco@gmail.com>
2025-02-14 16:10:50 -0800

    docs(#57): Add comments for clarity

--- LeetCode/57-insert-interval/Solution.java
+++ LeetCode/57-insert-interval/Solution.java
@@ -7,11 +7,13 @@ class Solution {
     int i = 0;
     int n = intervals.length;
 
+    // Add all intervals that come before the new interval
     while (i < n && intervals[i][1] < newInterval[0]) {
         result.add(intervals[i]);
         i++;
     }
 
+    // Merge all overlapping intervals with the new interval
     while (i < n && intervals[i][0] <= newInterval[1]) {
         newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
         newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
@@ -19,6 +21,7 @@ class Solution {
     }
     result.add(newInterval);
 
+    // Add all intervals that come after the new interval
     while (i < n) {
         result.add(intervals[i]);
         i++;
