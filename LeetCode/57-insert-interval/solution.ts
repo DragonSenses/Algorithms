c9c75f61f5bce9fd3962abe3771618872e1155f5
DragonSenses <kendrickfrancisco@gmail.com>
2025-02-14 17:30:02 -0800

    feat(#57): Merge/insert interval w/ linear search

--- LeetCode/57-insert-interval/solution.ts
+++ LeetCode/57-insert-interval/solution.ts
@@ -1,3 +1,24 @@
 function insert(intervals: number[][], newInterval: number[]): number[][] {
-    
+  const result: number[][] = [];
+  let i = 0;
+  const n = intervals.length;
+
+  while (i < n && intervals[i][1] < newInterval[0]) {
+    result.push(intervals[i]);
+    i++;
+  }
+
+  while (i < n && intervals[i][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+    newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+    i++;
+  }
+  result.push(newInterval);
+
+  while (i < n) {
+    result.push(intervals[i]);
+    i++;
+  }
+
+  return result;
 };
