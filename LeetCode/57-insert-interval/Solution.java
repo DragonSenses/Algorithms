3241ada4a0098ea9c57e50d7f649582c20e1a035
DragonSenses <kendrickfrancisco@gmail.com>
2025-02-14 16:35:41 -0800

    feat(#57): Merge/insert intervals with lin search

--- LeetCode/57-insert-interval/Solution.java
+++ LeetCode/57-insert-interval/Solution.java
@@ -4,29 +4,25 @@ import java.util.List;
 class Solution {
   public int[][] insert(int[][] intervals, int[] newInterval) {
     List<int[]> result = new ArrayList<>();
-    int i = 0;
-    int n = intervals.length;
+    int i = 0, n = intervals.length;
 
-    // Add all intervals that come before the new interval
     while (i < n && intervals[i][1] < newInterval[0]) {
-        result.add(intervals[i]);
-        i++;
+      result.add(intervals[i]);
+      i++;
     }
 
-    // Merge all overlapping intervals with the new interval
     while (i < n && intervals[i][0] <= newInterval[1]) {
-        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-        i++;
+      newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+      newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+      i++;
     }
     result.add(newInterval);
 
-    // Add all intervals that come after the new interval
     while (i < n) {
-        result.add(intervals[i]);
-        i++;
+      result.add(intervals[i]);
+      i++;
     }
 
-    return result.stream().mapToInt(Integer::intValue).toArray();
+    return result.toArray(new int[result.size()][]);
   }
 }
