fe2559a786e47f9bb451c9dcc3f8a29fee0a3afa
DragonSenses <kendrickfrancisco@gmail.com>
2025-02-26 21:49:27 -0800

    feat(#52): Init sets to track cols & diagonals

--- LeetCode/52-n-queens-ii/solution.ts
+++ LeetCode/52-n-queens-ii/solution.ts
@@ -1,3 +1,8 @@
 function totalNQueens(n: number): number {
+  // Set to keep track of columns, diagonals, and anti-diagonals.
+  const cols = new Set<number>();
+  const diagonals = new Set<number>();
+  const antiDiagonals = new Set<number>();
 
+  return 0;
 }
