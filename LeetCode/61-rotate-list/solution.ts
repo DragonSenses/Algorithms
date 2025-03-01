99f8fab0b7e351b69f2d7b236ae5edde4aa1f069
DragonSenses <kendrickfrancisco@gmail.com>
2025-02-28 19:10:41 -0800

    feat(#61): Add ListNode for singly-linked list

--- LeetCode/61-rotate-list/solution.ts
+++ LeetCode/61-rotate-list/solution.ts
@@ -1,14 +1,20 @@
 /**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
+ * Class representing a singly-linked list node.
  */
+class ListNode {
+  val: number;
+  next: ListNode | null;
+
+  /**
+   * Creates a ListNode.
+   * @param val - The value of the node.
+   * @param next - The reference to the next node.
+   */
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
 
 function rotateRight(head: ListNode | null, k: number): ListNode | null {
     
