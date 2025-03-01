740df6c41331cdefd8f9559ea17838748f25e1ee
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-01 11:57:23 -0800

    feat(#61): Add ListNode class definition

--- LeetCode/61-rotate-list/Solution.java
+++ LeetCode/61-rotate-list/Solution.java
@@ -1,5 +1,15 @@
-class Solution {
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode(int val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+public class Solution {
   public ListNode rotateRight(ListNode head, int k) {
-      
+
   }
 }
