623b1c29742949b6741fb6c3cb13fff257823b61
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-07 19:25:25 -0800

    docs(#80): Add comments and Javadoc for clarity

--- LeetCode/80-remove-duplicates-from-sorted-array-ii/Solution.java
+++ LeetCode/80-remove-duplicates-from-sorted-array-ii/Solution.java
@@ -1,4 +1,16 @@
+/**
+ * This class provides a solution for removing duplicates from a sorted array, allowing a maximum of
+ * two occurrences of each element.
+ */
 public class Solution {
+
+  /**
+   * Removes duplicates from a sorted array, allowing at most two occurrences of each element.
+   * Modifies the array in place and returns the length of the modified array.
+   *
+   * @param nums The sorted array of integers.
+   * @return The length of the modified array after removing unwanted duplicates.
+   */
   public int removeDuplicates(int[] nums) {
     if (nums.length == 0) {
       return 0;
@@ -7,7 +19,7 @@ public class Solution {
     // Initialize pointers i and j
     int i = 1, j = 1;
 
-    // Initialize variable count to track of the number of occurrences of each element
+    // Initialize variable count to track the number of occurrences of each element
     int count = 1;
 
     // Iterate through the array
