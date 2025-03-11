c9d538447b9c4d00792311d8ef3bcb37625a63e7
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-10 21:13:45 -0700

    feat(#62): Set first row paths to 1

--- LeetCode/62-unique-paths/Solution.java
+++ LeetCode/62-unique-paths/Solution.java
@@ -7,6 +7,11 @@ class Solution {
     for (int i = 0; i < m; i++) {
       dp[i][0] = 1;
     }
+    // Set the number of paths to 1 for the first row
+    for (int j = 0; j < n; j++) {
+      dp[0][j] = 1;
+    }
+
     return 0;
   }
 }
