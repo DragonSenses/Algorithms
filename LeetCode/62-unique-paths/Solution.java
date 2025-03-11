d5448e90d562aa12a770783ef81aff4d7a44131d
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-10 21:05:35 -0700

    feat(#62): Set first column paths to 1

--- LeetCode/62-unique-paths/Solution.java
+++ LeetCode/62-unique-paths/Solution.java
@@ -3,6 +3,10 @@ class Solution {
     // Initialize a 2D array dp with dimensions m x n
     int[][] dp = new int[m][n];
 
+    // Set the number of paths to 1 for the first column
+    for (int i = 0; i < m; i++) {
+      dp[i][0] = 1;
+    }
     return 0;
   }
 }
