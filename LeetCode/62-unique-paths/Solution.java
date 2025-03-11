99f4b204dd2bba362c2a1b256c806cde9f78c99b
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-10 21:04:14 -0700

    feat(#62): Initialize DP table for uniquePaths

--- LeetCode/62-unique-paths/Solution.java
+++ LeetCode/62-unique-paths/Solution.java
@@ -1,5 +1,8 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        
-    }
+  public int uniquePaths(int m, int n) {
+    // Initialize a 2D array dp with dimensions m x n
+    int[][] dp = new int[m][n];
+
+    return 0;
+  }
 }
