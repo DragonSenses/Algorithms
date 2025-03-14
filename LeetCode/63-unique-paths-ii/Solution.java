fdc16e04ffaf72febc2775c01089e67364e015b9
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-14 16:17:13 -0700

    feat(#63): Retrieve dimensions of obstacleGrid

--- LeetCode/63-unique-paths-ii/Solution.java
+++ LeetCode/63-unique-paths-ii/Solution.java
@@ -1,5 +1,8 @@
 class Solution {
   public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-      
+    int row = obstacleGrid.length;
+    int col = obstacleGrid[0].length;
+
+    return 0;
   }
 }
