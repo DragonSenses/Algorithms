e70ecafe19619dad34c7225e5e2d005e5565b737
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-15 12:39:18 -0700

    feat(#63): Copy prev cell value if no obstacle

--- LeetCode/63-unique-paths-ii/Solution.java
+++ LeetCode/63-unique-paths-ii/Solution.java
@@ -9,6 +9,11 @@ class Solution {
 
     obstacleGrid[0][0] = 1;
 
+    // Initialize first row
+    for (int i = 1; i < col; i++) {
+      obstacleGrid[0][i] = obstacleGrid[0][i] == 1 ? 0 : obstacleGrid[0][i - 1];
+    }
+
     return 0;
   }
 }
