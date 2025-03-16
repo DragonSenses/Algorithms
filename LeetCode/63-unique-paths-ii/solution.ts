4666899458beaa6b730826ec53caff36f52486b4
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-16 13:48:21 -0700

    feat(#63): Retrieve obstacle grid dimensions

--- LeetCode/63-unique-paths-ii/solution.ts
+++ LeetCode/63-unique-paths-ii/solution.ts
@@ -1,3 +1,6 @@
 function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
-    
+  const rows: number = obstacleGrid.length;
+  const cols: number = obstacleGrid[0].length;
+
+  return 0;
 };
