34d4f084a00cb483343187471e37754a3747b31a
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-16 13:52:33 -0700

    feat(#63): Handle blocked start with early exit

--- LeetCode/63-unique-paths-ii/solution.ts
+++ LeetCode/63-unique-paths-ii/solution.ts
@@ -2,5 +2,10 @@ function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
   const rows: number = obstacleGrid.length;
   const cols: number = obstacleGrid[0].length;
 
+  // If the starting cell is an obstacle, no paths are possible
+  if (obstacleGrid[0][0] === 1) {
+    return 0;
+  }
+
   return 0;
 };
