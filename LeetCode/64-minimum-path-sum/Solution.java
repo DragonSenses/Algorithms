eef59b68927084de157b7dd5827240344940817b
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-18 17:27:27 -0700

    feat(#64): Add base method signature for recursion

--- LeetCode/64-minimum-path-sum/Solution.java
+++ LeetCode/64-minimum-path-sum/Solution.java
@@ -1,5 +1,9 @@
-class Solution {
+public class Solution {
+  public int computePathSum(int[][] grid, int i, int j) {
+    return 0;
+  }
+
   public int minPathSum(int[][] grid) {
-      
+    return computePathSum(grid, 0, 0);
   }
 }
