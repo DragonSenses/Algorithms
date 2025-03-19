fb9d9f1a6165c838289858c1ae7b641ab572a5ce
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-19 13:28:25 -0700

    feat(#64): Handle bottom-right corner base case

--- LeetCode/64-minimum-path-sum/solution.ts
+++ LeetCode/64-minimum-path-sum/solution.ts
@@ -5,6 +5,10 @@ function minPathSum(grid: number[][]): number {
       if (i >= grid.length || j >= grid[0].length) {
           return Infinity; // Treat as invalid path
       }
+      // Base case: Reached bottom-right corner
+      if (i === grid.length - 1 && j === grid[0].length - 1) {
+          return grid[i][j];
+      }
 
   };
 
