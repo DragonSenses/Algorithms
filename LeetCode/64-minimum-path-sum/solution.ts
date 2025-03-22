af89dc4249917435b2a08f6e5a7a8c1f07f30442
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-21 18:02:59 -0700

    feat(#64): Initialize 2D matrix DP table

--- LeetCode/64-minimum-path-sum/solution.ts
+++ LeetCode/64-minimum-path-sum/solution.ts
@@ -2,5 +2,8 @@ function minPathSum(grid: number[][]): number {
   const rows = grid.length;
   const cols = grid[0].length;
 
+  // Step 1: Initialize a DP table
+  const dp: number[][] = Array.from({ length: rows }, () => Array(cols).fill(0));
+
   return 0;
 }
