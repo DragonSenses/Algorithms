39c9e76afa290b9eb6b90daa4e3b02d43fc73bd1
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-24 11:22:58 -0700

    feat(#64): Use nested loop to traverse the grid

--- LeetCode/64-minimum-path-sum/Solution.java
+++ LeetCode/64-minimum-path-sum/Solution.java
@@ -4,6 +4,13 @@ public class Solution {
     int rows = grid.length;
     int cols = grid[0].length;
 
+    // Step 2: Traverse the grid and update it in-place
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+
+      }
+    }
+
     // The bottom-right cell now contains the minimum path sum
     return grid[rows - 1][cols - 1];
   }
