e64e7d08e10b9261b7fad776a963e6c752e7cfe8
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-24 11:49:25 -0700

    refactor(#64): Simplify expressions in cell updates

--- LeetCode/64-minimum-path-sum/solution.ts
+++ LeetCode/64-minimum-path-sum/solution.ts
@@ -4,18 +4,18 @@ function minPathSum(grid: number[][]): number {
 
   // Step 3: Populate the last row
   for (let j = cols - 2; j >= 0; j--) {
-    grid[rows - 1][j] = grid[rows - 1][j] + grid[rows - 1][j + 1];
+    grid[rows - 1][j] += grid[rows - 1][j + 1];
   }
 
   // Step 4: Populate the last column
   for (let i = rows - 2; i >= 0; i--) {
-    grid[i][cols - 1] = grid[i][cols - 1] + grid[i + 1][cols - 1];
+    grid[i][cols - 1] += grid[i + 1][cols - 1];
   }
 
   // Step 5: Traverse the grid backwards to fill the grid table
   for (let i = rows - 2; i >= 0; i--) {
     for (let j = cols - 2; j >= 0; j--) {
-      grid[i][j] = grid[i][j] + Math.min(grid[i + 1][j], grid[i][j + 1]);
+      grid[i][j] += Math.min(grid[i + 1][j], grid[i][j + 1]);
     }
   }
 
