f19b2679146e41b26fa16f95d4b1067ae527b240
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-27 18:16:03 -0700

    feat(#102): Create list to store nodes by level

--- LeetCode/102-binary-tree-level-order-traversal/Solution.java
+++ LeetCode/102-binary-tree-level-order-traversal/Solution.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
+  // List to hold the levels
+  List<List<Integer>> levels = new ArrayList<>();
+
   public List<List<Integer>> levelOrder(TreeNode root) {
-      
+    return 0;
   }
+
 }
