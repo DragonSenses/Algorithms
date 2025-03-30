c1d48f0a9de714c84d1d1dc49c91a073b398f4b6
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-30 16:31:11 -0700

    feat(#102): Initialize levels list for traversal

--- LeetCode/102-binary-tree-level-order-traversal/Solution2.java
+++ LeetCode/102-binary-tree-level-order-traversal/Solution2.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution2 {
   public List<List<Integer>> levelOrder(TreeNode root) {
-      
+    List<List<Integer>> levels = new ArrayList<>(); // Initialize levels list
+
+    return null;
   }
 }
