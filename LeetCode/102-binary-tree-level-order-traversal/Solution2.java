8670c5e0846956191a795867bdc70e4ab3e71754
DragonSenses <kendrickfrancisco@gmail.com>
2025-03-30 16:36:45 -0700

    feat(#102): Handle empty tree edge case

--- LeetCode/102-binary-tree-level-order-traversal/Solution2.java
+++ LeetCode/102-binary-tree-level-order-traversal/Solution2.java
@@ -5,6 +5,11 @@ class Solution2 {
   public List<List<Integer>> levelOrder(TreeNode root) {
     List<List<Integer>> levels = new ArrayList<>(); // Initialize levels list
 
+    // Edge case: If the tree is empty, return an empty list
+    if (root == null) {
+      return levels;
+    }
+
     return null;
   }
 }
