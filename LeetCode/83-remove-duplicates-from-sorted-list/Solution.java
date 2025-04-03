07b8c0748998aae6037ae6e91a7d6ca4befd8172
DragonSenses <kendrickfrancisco@gmail.com>
2025-04-02 17:16:52 -0700

    feat(#83): Handle edge case for empty list

--- LeetCode/83-remove-duplicates-from-sorted-list/Solution.java
+++ LeetCode/83-remove-duplicates-from-sorted-list/Solution.java
@@ -1,5 +1,11 @@
 class Solution {
   public ListNode deleteDuplicates(ListNode head) {
-      
+    
+    // Edge case: Empty list
+    if (head == null) {
+      return null;
+    }
+
+    return head;
   }
 }
