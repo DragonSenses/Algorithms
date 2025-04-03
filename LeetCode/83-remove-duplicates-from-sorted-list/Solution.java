8d3287d1124bf9a81ebc3b290948516d939c1e06
DragonSenses <kendrickfrancisco@gmail.com>
2025-04-02 17:20:44 -0700

    feat(#83): Add logic to remove duplicates in list

--- LeetCode/83-remove-duplicates-from-sorted-list/Solution.java
+++ LeetCode/83-remove-duplicates-from-sorted-list/Solution.java
@@ -9,7 +9,11 @@ class Solution {
     ListNode current = head;
 
     while(current != null && current.next != null) {
-
+      if (current.val == current.next.val) {
+        current.next = current.next.next;
+      } else {
+        current = current.next;
+      }
     }
 
     return head;
