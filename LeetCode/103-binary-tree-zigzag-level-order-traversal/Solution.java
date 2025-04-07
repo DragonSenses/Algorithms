c635083a239b896c37c8924947d055b493f1f949
DragonSenses <kendrickfrancisco@gmail.com>
2025-04-06 18:33:58 -0700

    feat(#103): Add recursive traverse function

--- LeetCode/103-binary-tree-zigzag-level-order-traversal/Solution.java
+++ LeetCode/103-binary-tree-zigzag-level-order-traversal/Solution.java
@@ -5,8 +5,12 @@ class Solution {
   public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
     // Result list to store the zigzag level order traversal
     List<List<Integer>> result = new ArrayList<>();
-
+    // Start the recursive traversal
+    traverse(root, 0, result);
     return result;
   }
 
+  private void traverse(TreeNode node, int level, List<List<Integer>> result) {
+
+  }
 }
