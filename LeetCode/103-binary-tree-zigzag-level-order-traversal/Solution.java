c0e9fedfdbd286740226807b4ccd9b7e0a0912ed
DragonSenses <kendrickfrancisco@gmail.com>
2025-04-06 18:28:41 -0700

    feat(#103): Add list for zigzag level traversal

--- LeetCode/103-binary-tree-zigzag-level-order-traversal/Solution.java
+++ LeetCode/103-binary-tree-zigzag-level-order-traversal/Solution.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
   public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-      
+    // Result list to store the zigzag level order traversal
+    List<List<Integer>> result = new ArrayList<>();
+
+    return result;
   }
+
 }
