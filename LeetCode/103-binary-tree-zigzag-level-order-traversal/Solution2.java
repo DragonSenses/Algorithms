61b37c5f4e103bd9e116b26e48772b5aaecc0611
DragonSenses <kendrickfrancisco@gmail.com>
2025-04-08 20:34:51 -0700

    feat(#103): Handle edge case for empty tree

--- LeetCode/103-binary-tree-zigzag-level-order-traversal/Solution2.java
+++ LeetCode/103-binary-tree-zigzag-level-order-traversal/Solution2.java
@@ -4,7 +4,10 @@ import java.util.List;
 class Solution2 {
   public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
     List<List<Integer>> result = new ArrayList<>();
- 
+    if (root == null) {
+      return result; // Edge case: empty tree
+    }
+
     return result;
   }
 }
