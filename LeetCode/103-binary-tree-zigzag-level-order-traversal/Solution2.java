6ef8066fce72e2bbeeb02f57901c408467447a14
DragonSenses <kendrickfrancisco@gmail.com>
2025-04-08 20:28:14 -0700

    feat(#103): Create result list for traversal

---
+++ LeetCode/103-binary-tree-zigzag-level-order-traversal/Solution2.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution2 {
+  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    List<List<Integer>> result = new ArrayList<>();
+ 
+    return result;
+  }
+}
