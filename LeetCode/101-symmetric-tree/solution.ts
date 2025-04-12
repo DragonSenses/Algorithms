3633c8963d706add8f59e022517cd69523273a13
DragonSenses <kendrickfrancisco@gmail.com>
2025-04-12 10:41:53 -0700

    feat(#101): Handle case for unequal node values

--- LeetCode/101-symmetric-tree/solution.ts
+++ LeetCode/101-symmetric-tree/solution.ts
@@ -27,6 +27,9 @@ function isMirror(left: TreeNode | null, right: TreeNode | null): boolean {
   if (left === null || right === null) {
       return false;
   }
+  if (left.val !== right.val) {
+      return false;
+  }
 
   return null;
 }
