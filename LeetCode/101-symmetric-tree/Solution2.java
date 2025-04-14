9e0f4d1c343353aa4f10b671452a54eb99bd5245
DragonSenses <kendrickfrancisco@gmail.com>
2025-04-14 13:01:22 -0700

    feat(#101): Add edge case for empty tree

--- LeetCode/101-symmetric-tree/Solution2.java
+++ LeetCode/101-symmetric-tree/Solution2.java
@@ -1,5 +1,12 @@
+
 public class Solution2 {
   public boolean isSymmetric(TreeNode root) {
-        
+    
+    // Empty tree is symmetric
+    if (root == null) {
+      return true;
+    }
+
+    return false;
   }
 }
