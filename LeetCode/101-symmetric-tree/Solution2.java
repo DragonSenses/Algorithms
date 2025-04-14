ccee68f195acaa1b50ff81ae69b83c7e1ffeb68e
DragonSenses <kendrickfrancisco@gmail.com>
2025-04-14 13:02:32 -0700

    feat(#101): Init deque & enqueue first node pair

--- LeetCode/101-symmetric-tree/Solution2.java
+++ LeetCode/101-symmetric-tree/Solution2.java
@@ -1,3 +1,5 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Solution2 {
   public boolean isSymmetric(TreeNode root) {
@@ -6,6 +8,9 @@ public class Solution2 {
     if (root == null) {
       return true;
     }
+
+    Deque<TreeNode[]> deque = new ArrayDeque<>();
+    deque.add(new TreeNode[] {root.left, root.right});
 
     return false;
   }
