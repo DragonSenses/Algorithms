aba63673de51818487659e70a6f68bb6566f61fd
DragonSenses <kendrickfrancisco@gmail.com>
2025-04-21 14:08:44 -0700

    feat(#60): Compute factorials from 0! to (n-1)!

--- LeetCode/60-permutation-sequence/Solution.java
+++ LeetCode/60-permutation-sequence/Solution.java
@@ -16,5 +16,9 @@ class Solution {
     int[] factorials = new int[n + 1];
     factorials[0] = 1;
 
+    for (int i = 1; i <= n; i++) {
+      factorials[i] = factorials[i - 1] * i;
+    }
+
   }
 }
