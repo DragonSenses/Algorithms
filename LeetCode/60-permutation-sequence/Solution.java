d2858875625360984120f82cb64a5d0f109f3b4f
DragonSenses <kendrickfrancisco@gmail.com>
2025-04-21 13:57:46 -0700

    feat(#60): Generate the input list from 1 to n

--- LeetCode/60-permutation-sequence/Solution.java
+++ LeetCode/60-permutation-sequence/Solution.java
@@ -6,6 +6,12 @@ class Solution {
   // @input k = kth sequence
   // label permutations in order
   public String getPermutation(int n, int k) {
+    // Generate input list 1 to n
+    List<Integer> nums = new ArrayList<>();
+    
+    for (int i = 1; i <= n; i++) {
+      nums.add(i);
+    }
 
   }
 }
