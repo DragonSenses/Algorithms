a3ff6151a18df07a007d4d6d1d60f76a4711b1bb
DragonSenses <kendrickfrancisco@gmail.com>
2025-04-23 13:06:46 -0700

    feat(#60): Initialize and precompute factorials

--- LeetCode/60-permutation-sequence/solution.ts
+++ LeetCode/60-permutation-sequence/solution.ts
@@ -1,4 +1,11 @@
 function getPermutation(n: number, k: number): string {
   // Generate input array with numbers [1, 2, ..., n]
   const nums: number[] = Array.from({ length: n }, (_, i) => i + 1);
+
+  // Compute factorials from 0! to (n-1)!
+  const factorials: number[] = [1];
+  for (let i = 1; i <= n; i++) {
+    factorials[i] = factorials[i - 1] * i;
+  }
+
 };
