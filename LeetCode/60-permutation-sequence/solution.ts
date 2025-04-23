461ffbfee1801bc504721d75413b4ad8bc13348d
DragonSenses <kendrickfrancisco@gmail.com>
2025-04-23 13:01:30 -0700

    feat(#60): Initialize input array from 1 to n

--- LeetCode/60-permutation-sequence/solution.ts
+++ LeetCode/60-permutation-sequence/solution.ts
@@ -1,3 +1,4 @@
 function getPermutation(n: number, k: number): string {
-    
+  // Generate input array with numbers [1, 2, ..., n]
+  const nums: number[] = Array.from({ length: n }, (_, i) => i + 1);
 };
