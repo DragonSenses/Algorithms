c010bb1c42a4e3a0051db525572545896f003949
DragonSenses <kendrickfrancisco@gmail.com>
2025-04-27 16:25:54 -0700

    feat(#65): Validate current character as digit

--- LeetCode/65-valid-number/Solution.java
+++ LeetCode/65-valid-number/Solution.java
@@ -3,5 +3,13 @@ class Solution {
     boolean seenDigit = false;
     boolean seenExponent = false;
     boolean seenDot = false;
+
+    for (int i = 0; i < s.length(); i++) {
+      char curr = s.charAt(i);
+
+      if (Character.isDigit(curr)) {
+        seenDigit = true;
+      }
+    }
   }
 }
