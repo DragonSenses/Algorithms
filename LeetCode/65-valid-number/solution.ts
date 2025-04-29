41db272d45f7fd581a7aebb2499b0a9755197f38
DragonSenses <kendrickfrancisco@gmail.com>
2025-04-28 17:42:33 -0700

    feat(#65): Process input string to get character

--- LeetCode/65-valid-number/solution.ts
+++ LeetCode/65-valid-number/solution.ts
@@ -2,4 +2,8 @@ function isNumber(s: string): boolean {
   let seenDigit = false;
   let seenExponent = false;
   let seenDot = false;
+
+  for (let i = 0; i < s.length; i++) {
+    const curr = s[i];
+  }
 };
