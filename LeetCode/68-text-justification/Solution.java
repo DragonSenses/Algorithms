c2af3b7fde2d3614bee32ff9508d1adfe748870c
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-02 17:03:23 -0700

    feat(#68): Initialize result list and index

--- LeetCode/68-text-justification/Solution.java
+++ LeetCode/68-text-justification/Solution.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
   public List<String> fullJustify(String[] words, int maxWidth) {
-      
+    List<String> ans = new ArrayList<>();
+    int i = 0;
+    return ans;
   }
 }
