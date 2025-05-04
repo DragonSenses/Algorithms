3f035c06e493f0227aa1061bbf6fc40be0beb78a
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-04 12:08:05 -0700

    feat(#68): Extract words fitting within maxWidth

--- LeetCode/68-text-justification/solution.ts
+++ LeetCode/68-text-justification/solution.ts
@@ -5,5 +5,14 @@ function fullJustify(words: string[], maxWidth: number): string[] {
 
 function getWords(i: number, words: string[], maxWidth: number): string[] {
   const currentLine: string[] = [];
+  let currLength = 0;
+
+  while (i < words.length && currLength + words[i].length <= maxWidth) {
+    currentLine.push(words[i]);
+    // Account for a space after each word
+    currLength += words[i].length + 1;
+    i++;
+  }
+
   return currentLine;
 }
