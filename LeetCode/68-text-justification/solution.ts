02a5f4e26c70e1ab7db51c82fa0f7dfa5dfcacf8
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-04 12:03:48 -0700

    feat(#68): Define subroutine to get words for line

--- LeetCode/68-text-justification/solution.ts
+++ LeetCode/68-text-justification/solution.ts
@@ -2,3 +2,8 @@ function fullJustify(words: string[], maxWidth: number): string[] {
   const result: string[] = [];
   return result;
 }
+
+function getWords(i: number, words: string[], maxWidth: number): string[] {
+  const currentLine: string[] = [];
+  return currentLine;
+}
