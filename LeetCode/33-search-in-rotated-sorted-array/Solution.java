49c2870410a0cff01643d79ae56dca39096ee7a7
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-07 12:55:59 -0700

    feat(#33): Define variables for binary search

--- LeetCode/33-search-in-rotated-sorted-array/Solution.java
+++ LeetCode/33-search-in-rotated-sorted-array/Solution.java
@@ -1,5 +1,10 @@
 class Solution {
   public int search(int[] nums, int target) {
-      
+    // Declare variables
+    int mid = 0;
+    int left = 0;
+    int right = nums.length - 1;
+    int pivotIndex = -1;
+
   }
 }
