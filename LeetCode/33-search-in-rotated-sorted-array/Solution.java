6d739a366545a9266076a3e1a4da88d3e756cf57
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-08 12:11:50 -0700

    feat(#33): Implement binary search

--- LeetCode/33-search-in-rotated-sorted-array/Solution.java
+++ LeetCode/33-search-in-rotated-sorted-array/Solution.java
@@ -28,5 +28,20 @@ class Solution {
       right = pivotIndex - 1;
     }
 
+    // 3. Perform binary search
+    while (left <= right) {
+      mid = (left + right) / 2;
+      if (nums[mid] == target) {
+        // Target found
+        return mid;
+      } else if (nums[mid] < target){
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    // Target not found
+    return -1; 
   }
 }
