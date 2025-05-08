00f7eb39d6d06785536208115309928d3c15023b
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-08 12:40:17 -0700

    feat(#33): Locate pivot index (smallest element)

--- LeetCode/33-search-in-rotated-sorted-array/solution.ts
+++ LeetCode/33-search-in-rotated-sorted-array/solution.ts
@@ -1,3 +1,15 @@
 function search(nums: number[], target: number): number {
-    
+  let left = 0;
+  let right = nums.length - 1;
+
+  while(left < right) {
+    let mid = Math.floor(left + (right - left) / 2);
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  const pivotIndex = left;
 };
