ca65442709e488d19a735f4748047bdd341e83cc
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-11 12:36:23 -0700

    feat(#33): Use single loop for rotated array

--- LeetCode/33-search-in-rotated-sorted-array/solution2.ts
+++ LeetCode/33-search-in-rotated-sorted-array/solution2.ts
@@ -7,22 +7,11 @@ function search(nums: number[], target: number): number {
   while (left <= right) {
     const mid = Math.floor(left + (right - left) / 2);
 
-    if (nums[mid] > nums[right]) {
-      left = mid + 1;
-    } else {
-      right = mid;
-    }
-  }
-
-  while (left <= right) {
-    const mid = Math.floor(left + (right - left) / 2);
+    // Found target
     if (nums[mid] === target) {
-      return mid; // Target found
-    } else if (nums[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
+      return mid;
     }
+
   }
 
   return -1; // Target not found
