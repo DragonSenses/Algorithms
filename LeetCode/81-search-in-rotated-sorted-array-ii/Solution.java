39b42b645096b2bae459150031def9ce5a95a6a4
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-14 12:36:48 -0700

    feat(#81): Init pointers and safely compute mid

--- LeetCode/81-search-in-rotated-sorted-array-ii/Solution.java
+++ LeetCode/81-search-in-rotated-sorted-array-ii/Solution.java
@@ -1,5 +1,11 @@
 class Solution {
   public boolean search(int[] nums, int target) {
+    int start = 0, end = nums.length - 1;
+
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+
       
+    }
   }
 }
