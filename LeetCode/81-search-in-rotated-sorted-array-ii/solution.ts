380c303e322d6a734bd6ee6368de99b3a20563f5
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-15 13:21:36 -0700

    feat(#81): Init pointers and safely compute mid

--- LeetCode/81-search-in-rotated-sorted-array-ii/solution.ts
+++ LeetCode/81-search-in-rotated-sorted-array-ii/solution.ts
@@ -1,3 +1,9 @@
 function search(nums: number[], target: number): boolean {
-    
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor(start + (end - start) /2);
+  }
+
 };
