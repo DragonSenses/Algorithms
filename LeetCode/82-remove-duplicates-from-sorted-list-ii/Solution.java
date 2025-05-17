d34be01879bc14e24e909eba02bc490d0850afa8
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-17 07:01:02 -0700

    feat(#82): Initialize sentinel and previous node

--- LeetCode/82-remove-duplicates-from-sorted-list-ii/Solution.java
+++ LeetCode/82-remove-duplicates-from-sorted-list-ii/Solution.java
@@ -1,15 +1,8 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    public ListNode deleteDuplicates(ListNode head) {
-        
-    }
+  public ListNode deleteDuplicates(ListNode head) {
+    ListNode sentinel = new ListNode(0, head);
+    ListNode prevNode = sentinel;  // Tracks last distinct node before duplicates
+
+    return sentinel.next;
+  }
 }
