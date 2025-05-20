f7834a15dbb427c4f7de2bf1a42d07b404cba0d9
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-19 18:16:29 -0700

    feat(#82): Add ListNode for singly-linked list

--- LeetCode/82-remove-duplicates-from-sorted-list-ii/solution.ts
+++ LeetCode/82-remove-duplicates-from-sorted-list-ii/solution.ts
@@ -1,14 +1,11 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
 
 function deleteDuplicates(head: ListNode | null): ListNode | null {
     
