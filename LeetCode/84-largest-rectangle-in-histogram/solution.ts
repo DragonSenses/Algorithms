a8929e656525b1c365c652f706a42104243490c8
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-21 14:35:15 -0700

    feat(#84): Iterate over all possible starting bars

--- LeetCode/84-largest-rectangle-in-histogram/solution.ts
+++ LeetCode/84-largest-rectangle-in-histogram/solution.ts
@@ -1,3 +1,11 @@
 function largestRectangleArea(heights: number[]): number {
-    
+  let maxArea = 0;
+  let n = heights.length;
+
+  // Iterate over all possible starting bars
+  for (let i = 0; i < n; i++) {
+    let minHeight = heights[i];
+  }
+  
+  return maxArea;
 };
