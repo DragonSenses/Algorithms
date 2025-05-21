3ab36ca532f0ed404c2a258727b8902991e7956d
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-21 13:32:44 -0700

    feat(#84): Iterate over all possible starting bars

--- LeetCode/84-largest-rectangle-in-histogram/Solution.java
+++ LeetCode/84-largest-rectangle-in-histogram/Solution.java
@@ -1,8 +1,15 @@
 class Solution {
   public int largestRectangleArea(int[] heights) {
     int maxArea = 0;
-    for (int i = 0; i < heights.size() i++){
-      for (int j)
+    int n = heights.length;
+
+    // Iterate over all possible starting bars
+    for (int i = 0; i < n; i++) {
+      for (int j = i; j < n; j++) {
+
+      }
     }
+
+    return maxArea;
   }
 }
