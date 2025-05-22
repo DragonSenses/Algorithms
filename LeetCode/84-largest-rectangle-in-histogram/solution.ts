ff053bf8e0785d0fe572200dbe07052fd1f4d023
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-22 12:40:36 -0700

    feat(#84): Compute and track maximum area

--- LeetCode/84-largest-rectangle-in-histogram/solution.ts
+++ LeetCode/84-largest-rectangle-in-histogram/solution.ts
@@ -8,7 +8,13 @@ function largestRectangleArea(heights: number[]): number {
 
     // Expand the rectangle by iterating over possible ending bars
     for (let j = i; j < n; j++) {
+      // Maintain minimum height in range
+      minHeight = Math.min(minHeight, heights[j]); 
 
+      let width = j - i + 1; // Calculate width
+
+      // Compute and track maximum area
+      maxArea = Math.max(maxArea, minHeight * width); 
     }
   }
   
