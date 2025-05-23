3abe6d2425b5392415d0b57d7579e2eda75bab71
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-23 13:56:54 -0700

    feat(#84): Process previous taller bars

--- LeetCode/84-largest-rectangle-in-histogram/Solution.java
+++ LeetCode/84-largest-rectangle-in-histogram/Solution.java
@@ -9,6 +9,12 @@ class Solution {
     for (int i = 0; i < n; i++) {
       // Use 0 height for the imaginary right boundary
       int currentHeight = (i == n) ? 0 : heights[i];
+
+      // Pop elements while the current bar is shorter than stack top
+      while (!stack.isEmpty() && currentHeight < heights[stack.peek()]) {
+
+      }
+
     }
 
     return maxArea;
