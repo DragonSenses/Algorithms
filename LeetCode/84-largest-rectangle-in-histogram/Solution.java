851eb4e4405cbe3010234fe0f067b1681558fc85
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-23 13:50:35 -0700

    refactor(#84): Remove brute force, implement stack

--- LeetCode/84-largest-rectangle-in-histogram/Solution.java
+++ LeetCode/84-largest-rectangle-in-histogram/Solution.java
@@ -1,20 +1,13 @@
+import java.util.Stack;
+
 class Solution {
   public int largestRectangleArea(int[] heights) {
+    Stack<Integer> stack = new Stack<>();
     int maxArea = 0;
     int n = heights.length;
 
-    // Iterate over all possible starting bars
     for (int i = 0; i < n; i++) {
-      int minHeight = heights[i];
 
-      // Expand the rectangle by iterating over possible ending bars
-      for (int j = i; j < n; j++) {
-        minHeight = Math.min(minHeight, heights[j]); // Maintain minimum height in range
-
-        // Compute the rectangle's area and update maxArea
-        int width = j - i + 1;
-        maxArea = Math.max(maxArea, minHeight * width);
-      }
     }
 
     return maxArea;
