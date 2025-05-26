8641dcc536391a83e11297eb9374f4165a50e579
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-26 12:52:57 -0700

    feat(#85): Implement validRectangle function

--- LeetCode/85-maximal-rectangle/Solution2.java
+++ LeetCode/85-maximal-rectangle/Solution2.java
@@ -7,7 +7,7 @@ class Solution2 {
       for (int y1 = 0; y1 < m; y1++) {
         for (int x2 = x1; x2 < n; x2++) {
           for (int y2 = y1; y2 < m; y2++) {
-
+            
           }
         }
       }
@@ -15,5 +15,14 @@ class Solution2 {
     return maxArea;
   }
 
-
+  private boolean isValidRectangle(char[][] matrix, int x1, int y1, int x2, int y2) {
+    for (int i = x1; i <= x2; i++) {
+      for (int j = y1; j <= y2; j++) {
+        if (matrix[i][j] == '0') {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 }
