596cad49520cd20f63aa1ae3c57405b779a281c9
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-26 12:49:13 -0700

    feat(#85): Loop over all (x1,y1) coordinate pair

---
+++ LeetCode/85-maximal-rectangle/Solution2.java
@@ -0,0 +1,15 @@
+class Solution2 {
+  public int maximalRectangle(char[][] matrix) {
+    int n = matrix.length, m = matrix[0].length;
+    int maxArea = 0;
+
+    for (int x1 = 0; x1 < n; x1++) {
+      for (int y1 = 0; y1 < m; y1++) {
+
+      }
+    }
+    return maxArea;
+  }
+
+
+}
