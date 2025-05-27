4791c0c37cc91cf1fd2edf73f43657534a3115e1
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-27 13:23:04 -0700

    feat(#85): Handle edge case for empty matrix

--- LeetCode/85-maximal-rectangle/Solution3.java
+++ LeetCode/85-maximal-rectangle/Solution3.java
@@ -1,5 +1,9 @@
 class Solution3 {
   public int maximalRectangle(char[][] matrix) {
+    if (matrix.length == 0) {
+      return 0;
+    }
+    
     int n = matrix.length, m = matrix[0].length;
     int maxArea = 0;
     int[][] dp = new int[n][m]; // Stores max width of '1's
