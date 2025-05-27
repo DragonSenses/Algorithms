b4d0db52e7a8ff41a85a91c52e6fcc9f359e75f0
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-27 13:16:41 -0700

    feat(#85): Initialize DP table and track variables

---
+++ LeetCode/85-maximal-rectangle/Solution3.java
@@ -0,0 +1,8 @@
+class Solution3 {
+  public int maximalRectangle(char[][] matrix) {
+    int n = matrix.length, m = matrix[0].length;
+    int maxArea = 0;
+    int[][] dp = new int[n][m]; // Stores max width of '1's
+    return maxArea;
+  }
+}
