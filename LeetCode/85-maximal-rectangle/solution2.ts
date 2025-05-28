e0aab83bbcae4daf2079732d54323594d8f0e45b
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-28 14:02:47 -0700

    feat(#85): Initialize DP table and variables

---
+++ LeetCode/85-maximal-rectangle/solution2.ts
@@ -0,0 +1,8 @@
+
+function maximalRectangle(matrix: string[][]): number {
+  const n = matrix.length, m = matrix[0].length;
+  let maxArea = 0;
+  const dp: number[][] = Array.from({ length: n }, () => Array(m).fill(0));
+
+  return maxArea;
+}
