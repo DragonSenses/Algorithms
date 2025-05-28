36c78378c9b83c3e2421f3e0a4296e9b45f3b23b
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-28 14:03:15 -0700

    feat(#85): Handle edge case for empty matrix

--- LeetCode/85-maximal-rectangle/solution2.ts
+++ LeetCode/85-maximal-rectangle/solution2.ts
@@ -1,5 +1,6 @@
 
 function maximalRectangle(matrix: string[][]): number {
+  if (matrix.length === 0) return 0;
   const n = matrix.length, m = matrix[0].length;
   let maxArea = 0;
   const dp: number[][] = Array.from({ length: n }, () => Array(m).fill(0));
