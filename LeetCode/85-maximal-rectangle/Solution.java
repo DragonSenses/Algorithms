63a35e7f3807c3bea0c71d75638c557f521f2c83
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-29 15:29:29 -0700

    feat(#85): Update heights array based on row value

--- LeetCode/85-maximal-rectangle/Solution.java
+++ LeetCode/85-maximal-rectangle/Solution.java
@@ -8,6 +8,13 @@ class Solution {
     int[] heights = new int[m]; // Histogram heights
     int maxArea = 0;
 
+    for (char[] row : matrix) {
+      // Update heights array
+      for (int j = 0; j < m; j++) {
+        heights[j] = (row[j] == '1') ? heights[j] + 1 : 0;
+      }
+    }
+
     return maxArea;
   }
 }
