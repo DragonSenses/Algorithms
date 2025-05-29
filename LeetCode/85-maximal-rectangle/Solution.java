c094aa7a6bc1e21cb5d3d468288055d4cfd9dc98
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-29 15:21:35 -0700

    feat(#85): Init heights, maxArea, and track vars

--- LeetCode/85-maximal-rectangle/Solution.java
+++ LeetCode/85-maximal-rectangle/Solution.java
@@ -3,5 +3,11 @@ class Solution {
     if (matrix.length == 0) {
       return 0;
     }
+
+    int n = matrix.length, m = matrix[0].length;
+    int[] heights = new int[m]; // Histogram heights
+    int maxArea = 0;
+
+    return maxArea;
   }
 }
