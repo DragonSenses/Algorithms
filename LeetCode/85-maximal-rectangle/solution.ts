484934fce5eb3002b6e96c283948dc074d5dda78
DragonSenses <kendrickfrancisco@gmail.com>
2025-05-31 16:28:03 -0700

    feat(#85): Initialize heights and maxArea

--- LeetCode/85-maximal-rectangle/solution.ts
+++ LeetCode/85-maximal-rectangle/solution.ts
@@ -2,5 +2,8 @@ function maximalRectangle(matrix: string[][]): number {
   if (matrix.length === 0) {
     return 0;
   }
-  
-};
+
+  const m = matrix[0].length;
+  let maxArea = 0;
+  const heights: number[] = new Array(m).fill(0); // Column heights
+}
