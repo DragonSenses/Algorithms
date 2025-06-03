ee0a7d75965883b3c504b2a7854ce5efde716ca8
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-03 12:14:43 -0700

    feat(#87): Check possible scramble conditions

--- LeetCode/87-scramble-string/Solution.java
+++ LeetCode/87-scramble-string/Solution.java
@@ -18,7 +18,7 @@ class Solution {
 
     // Generate a unique key for memoization using both strings
     String key = s1 + "_" + s2;
-    
+
     // If result for this pair of strings is already computed, return the stored value
     if (cache.containsKey(key)) {
       return cache.get(key);
@@ -26,7 +26,7 @@ class Solution {
 
     int n = s1.length();
     int[] count = new int[26]; // Frequency array to compare character counts in s1 and s2
-    
+
     // Count frequency of characters in both strings
     for (int i = 0; i < n; i++) {
       count[s1.charAt(i) - 'a']++; // Increment count for s1's characters
@@ -34,10 +34,23 @@ class Solution {
     }
 
     // If character counts mismatch, s2 cannot be a scrambled version of s1
-    for (int c: count) {
+    for (int c : count) {
       if (c != 0) {
         cache.put(key, false);
         return false;
+      }
+    }
+
+    // Iterate over possible split points of the string
+    for (int len = 1; len < n; len++) {
+      // Check two possible scramble conditions:
+      // 1. No swap: First half of s1 matches first half of s2, second half matches second half
+      // 2. Swap: First half of s1 matches second half of s2, and vice versa
+      if ((isScramble(s1.substring(0, len), s2.substring(0, len))
+          && isScramble(s1.substring(len), s2.substring(len)))
+          || (isScramble(s1.substring(0, len), s2.substring(n - len))
+              && isScramble(s1.substring(len), s2.substring(0, n - len)))) {
+        return true;
       }
     }
 
