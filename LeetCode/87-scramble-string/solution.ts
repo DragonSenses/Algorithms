e5bc257b84ec9bb2719209cb41537c2d6a733bfb
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-04 12:43:34 -0700

    feat(#87): Create unique key for memoization

--- LeetCode/87-scramble-string/solution.ts
+++ LeetCode/87-scramble-string/solution.ts
@@ -4,4 +4,7 @@ function isScramble(s1: string, s2: string): boolean {
 
   // If lengths differ, it's impossible for s2 to be a scrambled version of s1
   if (s1.length !== s2.length) return false;
+
+  // Generate a unique memoization key based on both strings
+  const key = `${s1}_${s2}`;
 };
