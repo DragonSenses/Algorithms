85946f80c390cc6b9d2346bb3bd04d572aacacc2
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-07 09:50:03 -0700

    feat(#89): Define sequence list and visited set

---
+++ LeetCode/89-gray-code/Solution2.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution2 {
+  public List<Integer> grayCode(int n) {
+    List<Integer> sequence = new ArrayList<>();
+    Set<Integer> visited = new HashSet<>();
+
+    return sequence;
+  }
+
+
+}
