ff14f32988fe7d8cf14a2f34470ed812cecc3fa1
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-10 13:06:12 -0700

    feat(#89): Initialize result list with starting 0

--- LeetCode/89-gray-code/Solution.java
+++ LeetCode/89-gray-code/Solution.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
   public List<Integer> grayCode(int n) {
-
+    List<Integer> result = new ArrayList<>();
+    result.add(0); // Initialize sequence with 0
   }
 }
