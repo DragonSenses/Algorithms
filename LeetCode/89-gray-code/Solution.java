fa9e60394c2fe797811633586995c2c5f6a2dfae
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-11 12:12:18 -0700

    feat(#89): Mirror sequence and apply bitwise mask

--- LeetCode/89-gray-code/Solution.java
+++ LeetCode/89-gray-code/Solution.java
@@ -9,6 +9,10 @@ class Solution {
     for (int i = 1; i <= n; i++) {
       int mask = 1 << (i - 1); // Set the (i-1)th bit using bit shift
 
+      // Reverse iterate over current sequence and apply mask
+      for (int j = result.size() - 1; j >= 0; j--) {
+        result.add(result.get(j) | mask); // Add prefix 1 to mirrored numbers
+      }
     }
 
   }
