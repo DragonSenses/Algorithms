30af8626c666a7124146a648d2b650ffc1301a8b
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-11 12:39:06 -0700

    feat(#89): Define mask for bit shifting

--- LeetCode/89-gray-code/solution.ts
+++ LeetCode/89-gray-code/solution.ts
@@ -1,5 +1,9 @@
 function grayCodeIterative(n: number): number[] {
   const result: number[] = [0]; // Initialize with 0
 
+  for (let i = 0; i < n; i++) {
+    const mask = 1 << i; // Define mask for bit shifting
+  }
+
   return result;
 }
