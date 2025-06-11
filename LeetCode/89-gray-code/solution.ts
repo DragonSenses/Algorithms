b48285946f02ad0cffcc38a1522cb66960e25684
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-11 12:41:19 -0700

    feat(#89): Reverse iterate & apply bitwise OR mask

--- LeetCode/89-gray-code/solution.ts
+++ LeetCode/89-gray-code/solution.ts
@@ -3,6 +3,11 @@ function grayCodeIterative(n: number): number[] {
 
   for (let i = 0; i < n; i++) {
     const mask = 1 << i; // Define mask for bit shifting
+
+    // Reverse iterate and apply bitwise OR to create mirrored sequence
+    for (let j = result.length - 1; j >= 0; j--) {
+      result.push(result[j] | mask);
+    }
   }
 
   return result;
