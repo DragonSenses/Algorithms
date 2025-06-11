1b9a669bf7da2b55655b9251824a9ec8d84ddddc
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-11 12:04:43 -0700

    feat(#89): Iterate bits & define shift-based mask

--- LeetCode/89-gray-code/Solution.java
+++ LeetCode/89-gray-code/Solution.java
@@ -5,5 +5,11 @@ class Solution {
   public List<Integer> grayCode(int n) {
     List<Integer> result = new ArrayList<>();
     result.add(0); // Initialize sequence with 0
+
+    for (int i = 1; i <= n; i++) {
+      int mask = 1 << (i - 1); // Set the (i-1)th bit using bit shift
+
+    }
+
   }
 }
