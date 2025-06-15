04fa0596a7bef51cbf9b05ef2e122a264419628c
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-15 12:33:09 -0700

    feat(#90): Declare variables & import dependencies

--- LeetCode/90-subsets-ii/Solution.java
+++ LeetCode/90-subsets-ii/Solution.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 class Solution {
   public List<List<Integer>> subsetsWithDup(int[] nums) {
     // Step 1: Sort nums to ensure consistent subset ordering
     Arrays.sort(nums);
+
+    int n = nums.length;
+    int maxSubsets = 1 << n; // Equivalent to 2^n
+    Set<String> seen = new HashSet<>();
+    List<List<Integer>> subsets = new ArrayList<>();
   }
 }
