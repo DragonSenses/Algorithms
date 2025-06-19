cddd45249791d9993fe22d19668411d07b96f8a1
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-19 13:04:32 -0700

    feat(#90): Declare variables & import dependencies

--- LeetCode/90-subsets-ii/Solution2.java
+++ LeetCode/90-subsets-ii/Solution2.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution2 {
+
   public List<List<Integer>> subsetsWithDup(int[] nums) {
     // Step 1: Sort the input array to ensure duplicates are grouped together
     Arrays.sort(nums);
+    
+    List<List<Integer>> subsets = new ArrayList<>();
+    subsets.add(new ArrayList<>()); // Start with an empty subset
+    
+    int subsetSize = 0; // Tracks the index where duplicates should start being added
+
   }
 }
