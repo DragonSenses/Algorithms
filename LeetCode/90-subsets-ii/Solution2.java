dd14ffd096dee8727dd0f81f918fd90095b5233c
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-19 13:01:51 -0700

    feat(#90): Sort input array before processing

---
+++ LeetCode/90-subsets-ii/Solution2.java
@@ -0,0 +1,6 @@
+class Solution2 {
+  public List<List<Integer>> subsetsWithDup(int[] nums) {
+    // Step 1: Sort the input array to ensure duplicates are grouped together
+    Arrays.sort(nums);
+  }
+}
