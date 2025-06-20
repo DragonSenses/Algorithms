cad2da9508a11dbb300f63f3fb24e7be2227cc2c
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-20 14:02:52 -0700

    feat(#90): Determine start index during loop

--- LeetCode/90-subsets-ii/solution2.ts
+++ LeetCode/90-subsets-ii/solution2.ts
@@ -5,4 +5,10 @@ function subsetsWithDup(nums: number[]): number[][] {
   const subsets: number[][] = [[]]; // Start with an empty subset
   let subsetSize = 0; // Tracks the index where duplicates should start being added
 
+  // Step 2: Iterate through the elements in nums
+  for (let i = 0; i < nums.length; i++) {
+    let startIndex = i > 0 && nums[i] === nums[i - 1] ? subsetSize : 0;
+
+  }
+
 }
