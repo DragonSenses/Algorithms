bd4cb5123d926557dd4bb43c285c59a483b671eb
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-21 12:00:44 -0700

    feat(#88): Manually deep copy first input array

--- LeetCode/88-merge-sorted-array/Solution.java
+++ LeetCode/88-merge-sorted-array/Solution.java
@@ -1,5 +1,9 @@
 class Solution {
   public void merge(int[] nums1, int m, int[] nums2, int n) {
-
+    // Step 1: Make a copy of the first m elements of nums1
+    int[] nums1Copy = new int[m];
+    for (int i = 0; i < m; i++) {
+      nums1Copy[i] = nums1[i];
+    }
   }
 }
