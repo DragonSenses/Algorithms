3bb313ee648062a746cdcad22db003f44c2ef26b
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-21 12:03:05 -0700

    refactor(#88): Use native array copy method

--- LeetCode/88-merge-sorted-array/Solution.java
+++ LeetCode/88-merge-sorted-array/Solution.java
@@ -2,8 +2,6 @@ class Solution {
   public void merge(int[] nums1, int m, int[] nums2, int n) {
     // Step 1: Make a copy of the first m elements of nums1
     int[] nums1Copy = new int[m];
-    for (int i = 0; i < m; i++) {
-      nums1Copy[i] = nums1[i];
-    }
+    System.arraycopy(nums1, 0, nums1Copy, 0, m);
   }
 }
