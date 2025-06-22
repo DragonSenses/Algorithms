c5a71f2834badcc7c61d2699dd55f53059d4041c
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-22 15:37:04 -0700

    feat(#88): Clone first m elements from nums1

--- LeetCode/88-merge-sorted-array/solution.ts
+++ LeetCode/88-merge-sorted-array/solution.ts
@@ -1,6 +1,6 @@
-/**
- Do not return anything, modify nums1 in-place instead.
- */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    
+  // Step 1: Copy the first m elements of nums1
+  const nums1Copy = nums1.slice(0, m);
+
+  
 };
