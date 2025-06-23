37786d0453e0a3bc0656a0b26764168a1073e583
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-23 13:17:40 -0700

    feat(#88): Merge from end using 3 pointers

--- LeetCode/88-merge-sorted-array/Solution2.java
+++ LeetCode/88-merge-sorted-array/Solution2.java
@@ -5,5 +5,14 @@ class Solution {
     int p2 = n - 1; // Last element in nums2
     int p = m + n - 1; // Last index in nums1
 
+    // Merge from the end
+    while (p1 >= 0 && p2 >= 0) {
+      if (nums1[p1] > nums2[p2]) {
+        nums1[p--] = nums1[p1--];
+      } else {
+        nums1[p--] = nums2[p2--];
+      }
+    }
+
   }
 }
