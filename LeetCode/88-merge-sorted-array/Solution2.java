0673321edde46302618b2d9a9c713fc9ef29cb86
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-23 13:14:26 -0700

    feat(#88): Initialize three pointers for merge

---
+++ LeetCode/88-merge-sorted-array/Solution2.java
@@ -0,0 +1,9 @@
+class Solution {
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    // Initialize pointers
+    int p1 = m - 1; // Last element in nums1
+    int p2 = n - 1; // Last element in nums2
+    int p = m + n - 1; // Last index in nums1
+
+  }
+}
