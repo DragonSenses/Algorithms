7b4aa21dd9569301959ea99f67131b63ef3c5840
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-24 13:20:44 -0700

    feat(#88): Initialize 3 pointers for merge

---
+++ LeetCode/88-merge-sorted-array/solution2.ts
@@ -0,0 +1,6 @@
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+  let p1 = m - 1; // Last element in nums1
+  let p2 = n - 1; // Last element in nums2
+  let p = m + n - 1; // Last position in nums1
+
+}
