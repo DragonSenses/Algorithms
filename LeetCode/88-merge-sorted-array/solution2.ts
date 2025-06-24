579956e210cfe6f9f2bf1a9ebde2622fa1da4eff
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-24 13:26:57 -0700

    feat(#88): Implement backward merge w/ 3 pointers

--- LeetCode/88-merge-sorted-array/solution2.ts
+++ LeetCode/88-merge-sorted-array/solution2.ts
@@ -3,4 +3,14 @@ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
   let p2 = n - 1; // Last element in nums2
   let p = m + n - 1; // Last position in nums1
 
+  // Merge from the back
+  while (p1 >= 0 && p2 >= 0) {
+    if (nums1[p1] > nums2[p2]) {
+      nums1[p--] = nums1[p1--];
+    } else {
+      nums1[p--] = nums2[p2--];
+    }
+  }
+
+
 }
