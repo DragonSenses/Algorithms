6e9859872423990425adee40370fb2e08552a13e
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-25 12:40:35 -0700

    feat(#92): Define pointers for list recursion

---
+++ LeetCode/92-reverse-linked-list-ii/Solution2.java
@@ -0,0 +1,12 @@
+class Solution2 {
+  // Global pointers for recursion
+  private ListNode leftPointer;
+  private boolean stop;
+
+  public ListNode reverseBetween(ListNode head, int left, int right) {
+    leftPointer = head;
+    stop = false;
+
+    return head;
+  }
+}
