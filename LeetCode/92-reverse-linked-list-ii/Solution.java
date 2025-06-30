31236adbc460c484e019ed97782b7ce8e727e039
DragonSenses <kendrickfrancisco@gmail.com>
2025-06-30 12:46:07 -0700

    feat(#92): Handle edge case of null input list

--- LeetCode/92-reverse-linked-list-ii/Solution.java
+++ LeetCode/92-reverse-linked-list-ii/Solution.java
@@ -1,15 +1,8 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
   public ListNode reverseBetween(ListNode head, int left, int right) {
+    // Handle empty list case
+    if (head == null) {
+      return null;
+    }
 
-  }
 }
