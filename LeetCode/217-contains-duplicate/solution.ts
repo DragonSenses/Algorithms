1e0bbd661e6c1918124603ab24a23fac6b89ed43
DragonSenses <kendrickfrancisco@gmail.com>
2025-07-03 14:45:21 -0700

    feat(#217): Detect duplicates with hash set

--- LeetCode/217-contains-duplicate/solution.ts
+++ LeetCode/217-contains-duplicate/solution.ts
@@ -1,3 +1,12 @@
 function containsDuplicate(nums: number[]): boolean {
-    
-};
+  const seen = new Set<number>();
+
+  for (const num of nums) {
+    if (seen.has(num)) {
+      return true;
+    }
+    seen.add(num);
+  }
+
+  return false;
+}
