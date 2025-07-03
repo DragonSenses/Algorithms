a592a156f9ff34497c15206854768110d49a1cea
DragonSenses <kendrickfrancisco@gmail.com>
2025-07-03 14:39:15 -0700

    feat(#217): Add hash set duplicate detection

--- LeetCode/217-contains-duplicate/Solution.java
+++ LeetCode/217-contains-duplicate/Solution.java
@@ -1,5 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        
+  public boolean containsDuplicate(int[] nums) {
+    Set<Integer> set = new HashSet<>(nums.length);
+
+    for (int x : nums) {
+      if (set.contains(x)) {
+        return true;
+      }
+      set.add(x);
     }
+    return false;
+  }
 }
