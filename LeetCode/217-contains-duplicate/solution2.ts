7792fe6ab6b2edea183ad238a9d29d289897502a
DragonSenses <kendrickfrancisco@gmail.com>
2025-07-05 10:46:08 -0700

    feat(#217): Detect duplicates with sort and scan

---
+++ LeetCode/217-contains-duplicate/solution2.ts
@@ -0,0 +1,9 @@
+function containsDuplicate(nums: number[]): boolean {
+  nums.sort((a, b) => a - b);
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) return true;
+  }
+
+  return false;
+};
