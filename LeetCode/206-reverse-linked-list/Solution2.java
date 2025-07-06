d5141f741557793f472cccba5a3f22e18b83d97f
DragonSenses <kendrickfrancisco@gmail.com>
2025-07-06 11:18:47 -0700

    feat(#206): Handle base case for 0 or 1 node

---
+++ LeetCode/206-reverse-linked-list/Solution2.java
@@ -0,0 +1,10 @@
+public class Solution2 {
+
+  public ListNode reverseList(ListNode head) {
+    // Base case: empty list or single node — already reversed
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+  }
+}
