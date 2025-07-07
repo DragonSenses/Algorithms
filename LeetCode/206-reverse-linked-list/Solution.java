6905fa30f700aa5d22939f13688cf4a977cae35a
DragonSenses <kendrickfrancisco@gmail.com>
2025-07-07 12:10:05 -0700

    feat(#206): Traverse list and reverse pointers

--- LeetCode/206-reverse-linked-list/Solution.java
+++ LeetCode/206-reverse-linked-list/Solution.java
@@ -2,7 +2,13 @@ class Solution {
   public ListNode reverseList(ListNode head) {
     ListNode prev = null;
     ListNode curr = head;
-    ListNode nextTemp = null;
+
+    while (curr != null) {
+      ListNode nextTemp = curr.next; // store next node
+      curr.next = prev; // reverse current node's pointer
+      prev = curr; // advance prev
+      curr = nextTemp; // advance curr
+    }
 
   }
 }
