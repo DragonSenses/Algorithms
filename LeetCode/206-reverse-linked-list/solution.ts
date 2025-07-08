6486b802a35dc34bacbd923a06b449b9d27c2c60
DragonSenses <kendrickfrancisco@gmail.com>
2025-07-08 13:06:42 -0700

    feat(#206): Initialize pointers and traverse list

--- LeetCode/206-reverse-linked-list/solution.ts
+++ LeetCode/206-reverse-linked-list/solution.ts
@@ -1,15 +1,20 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+class ListNode {
+  val: number;
+  next: ListNode | null;
+
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val ?? 0;
+    this.next = next ?? null;
+  }
+}
 
 function reverseList(head: ListNode | null): ListNode | null {
-    
-};
+  let prev: ListNode | null = null;
+  let curr: ListNode | null = head;
+
+  while (curr !== null) {
+    // Temporarily store the next node before modifying the link
+    const nextTemp: ListNode | null = curr.next;
+  }
+
+}
