cf5a7f0496dd724484e3700e8ca0b8ca32aabdb3
DragonSenses <kendrickfrancisco@gmail.com>
2025-07-09 12:59:04 -0700

    feat(#200): Handle null or empty input grid

--- LeetCode/200-number-of-islands/Solution.java
+++ LeetCode/200-number-of-islands/Solution.java
@@ -1,6 +1,11 @@
 class Solution {
 
   public int numIslands(char[][] grid) {
+    // Edge Case: Return 0 if grid is null or empty
+    if (grid == null || grid.length == 0 || grid[0].length == 0) {
+      return 0;
+    }
+
     int rows = grid.length;
     int cols = grid[0].length;
     int islandCount = 0;
