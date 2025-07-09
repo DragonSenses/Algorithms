d8bc4394276bbdf397ea2e6ef88759c5b9c16e5c
DragonSenses <kendrickfrancisco@gmail.com>
2025-07-09 12:46:27 -0700

    feat(#200): Set up grid dimensions & island count

--- LeetCode/200-number-of-islands/Solution.java
+++ LeetCode/200-number-of-islands/Solution.java
@@ -1,5 +1,12 @@
 class Solution {
-  public int numIslands(char[][] grid) {
 
+  public int numIslands(char[][] grid) {
+    int rows = grid.length;
+    int cols = grid[0].length;
+    int islandCount = 0;
+
+    // Return total number of distinct islands found
+    return islandCount;
   }
+
 }
