b13625996ab32efb58cebd37b437182471b575a0
DragonSenses <kendrickfrancisco@gmail.com>
2025-07-15 18:24:30 -0700

    feat(#200): Scan each cell in 2D grid

--- LeetCode/200-number-of-islands/solution.ts
+++ LeetCode/200-number-of-islands/solution.ts
@@ -3,4 +3,14 @@ function numIslands(grid: string[][]): number {
   if (grid.length === 0 || grid[0].length === 0) {
     return 0;
   }
+
+  const rows = grid.length;
+  const cols = grid[0].length;
+
+  // Scan the grid
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+
+    }
+  }
 };
