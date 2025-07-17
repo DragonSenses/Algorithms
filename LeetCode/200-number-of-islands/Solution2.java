11a0839bfd1697cea16c72cf6f27965522950fda
DragonSenses <kendrickfrancisco@gmail.com>
2025-07-17 16:08:05 -0700

    feat(#200): Iterate through each cell in 2D grid

--- LeetCode/200-number-of-islands/Solution2.java
+++ LeetCode/200-number-of-islands/Solution2.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution2 {
   public int numIslands(char[][] grid) {
     // Edge case check
     if (grid == null || grid.length == 0 || grid[0].length == 0) {
@@ -11,6 +11,11 @@ class Solution {
 
     // Direction vectors: up, down, left, right
     int[][] directions = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+
+    for (int r = 0; r < rows; r++) {
+      for (int c = 0; c < cols; c++) {
+      }
+    }
 
     return islandCount;
   }
