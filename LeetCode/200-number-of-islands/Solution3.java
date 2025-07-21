e71092cbdd5ef2f94b18fc1c5483dc022505adc7
DragonSenses <kendrickfrancisco@gmail.com>
2025-07-20 18:22:04 -0700

    feat(#200): Handle edge case for empty grid

--- LeetCode/200-number-of-islands/Solution3.java
+++ LeetCode/200-number-of-islands/Solution3.java
@@ -1,5 +1,10 @@
 class Solution3 {
   public int numIslands(char[][] grid) {
+    // Edge case: empty grid
+    if (grid == null || grid.length == 0 || grid[0].length == 0) {
+      return 0;
+    }
+
     int rows = grid.length;
     int cols = grid[0].length;
     int islandCount = 0;
@@ -8,6 +13,7 @@ class Solution3 {
     int[][] directions = {
       {-1, 0}, {1, 0}, {0, -1}, {0, 1}
     };
+
 
     return islandCount;
   }
