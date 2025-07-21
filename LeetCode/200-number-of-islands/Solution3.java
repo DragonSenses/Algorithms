93decc90bfddb67f56058b4b58e1b346bfa38699
DragonSenses <kendrickfrancisco@gmail.com>
2025-07-20 18:17:47 -0700

    feat(#200): Define four directional move vectors

--- LeetCode/200-number-of-islands/Solution3.java
+++ LeetCode/200-number-of-islands/Solution3.java
@@ -4,6 +4,11 @@ class Solution3 {
     int cols = grid[0].length;
     int islandCount = 0;
 
+    // Directions: up, down, left, right
+    int[][] directions = {
+      {-1, 0}, {1, 0}, {0, -1}, {0, 1}
+    };
+
     return islandCount;
   }
 }
