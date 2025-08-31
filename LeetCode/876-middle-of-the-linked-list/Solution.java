a33904f99833d9735197d26cdd390784a87f8dd3
DragonSenses <kendrickfrancisco@gmail.com>
2025-08-30 17:25:34 -0700

    feat(#876): Advance pointers to locate midpoint

--- LeetCode/876-middle-of-the-linked-list/Solution.java
+++ LeetCode/876-middle-of-the-linked-list/Solution.java
@@ -3,5 +3,11 @@ class Solution {
     ListNode slow = head;
     ListNode fast = head;
 
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    return slow;
   }
 }
