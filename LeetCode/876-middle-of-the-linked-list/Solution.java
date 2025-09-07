697f4738bf87cf3f6bc5409516f653a661e4e959
DragonSenses <kendrickfrancisco@gmail.com>
2025-09-07 14:54:46 -0700

    docs(#876): Add comments and Javadoc for clarity

--- LeetCode/876-middle-of-the-linked-list/Solution.java
+++ LeetCode/876-middle-of-the-linked-list/Solution.java
@@ -1,4 +1,12 @@
 class Solution {
+  /**
+   * Returns the middle node of a singly linked list using the two-pointer technique.
+   * This method employs a slow and fast pointer: - The slow pointer advances one node per iteration. - The fast pointer advances two nodes per iteration. When the fast pointer reaches the end of the list, the slow pointer will be at the middle.
+   * If the list has an even number of nodes, the method returns the second middle node.
+   *
+   * @param head the head of the singly linked list
+   * @return the middle node of the list (includes all subsequent nodes)
+   */
   public ListNode middleNode(ListNode head) {
     ListNode slow = head;
     ListNode fast = head;
