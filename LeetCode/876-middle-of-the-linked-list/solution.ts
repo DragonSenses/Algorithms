63eca3eccfaf31a90d4f9a60a1c4b827c094a8b1
DragonSenses <kendrickfrancisco@gmail.com>
2025-09-08 15:36:17 -0700

    feat(#876): Implement two pointer approach in TS

--- LeetCode/876-middle-of-the-linked-list/solution.ts
+++ LeetCode/876-middle-of-the-linked-list/solution.ts
@@ -1,15 +1,21 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+class ListNode {
+  val: number;
+  next: ListNode | null;
+
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val ?? 0;
+    this.next = next ?? null;
+  }
+}
 
 function middleNode(head: ListNode | null): ListNode | null {
-    
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+
+  while (fast != null && fast.next != null) {
+    slow = slow!.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
 };
