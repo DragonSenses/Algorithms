85a16877ac8670fa85d23cbef00f24c0edcaf99a
DragonSenses <kendrickfrancisco@gmail.com>
2025-09-13 14:54:53 -0700

    feat(#704): Implement binary search via two pointers

--- LeetCode/704-binary-search/Solution.java
+++ LeetCode/704-binary-search/Solution.java
@@ -1,5 +1,20 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        
+  public int search(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+
+    while (left <= right) {
+      int mid = (left + right) >>> 1;
+
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
     }
+
+    return -1; // target not found
+  }
 }
