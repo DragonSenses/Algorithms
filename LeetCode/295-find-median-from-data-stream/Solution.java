30c7c2b695dbff639104d1818f5bfd669259fce7
DragonSenses <kendrickfrancisco@gmail.com>
2025-09-15 15:30:31 -0700

    feat(#295): Implement naive approach to find median

--- LeetCode/295-find-median-from-data-stream/Solution.java
+++ LeetCode/295-find-median-from-data-stream/Solution.java
@@ -1,21 +1,28 @@
+import java.util.ArrayList;
+
 class MedianFinder {
+  ArrayList<Integer> list;
 
-    public MedianFinder() {
-        
-    }
-    
-    public void addNum(int num) {
-        
-    }
-    
-    public double findMedian() {
-        
-    }
+  // Initializes MedianFinder object
+  public MedianFinder() {
+    this.list = new ArrayList<>();
+  }
+
+  // Adds integer num from the data stream to data structure
+  public void addNum(int num) {
+    this.list.add(num);
+  }
+
+  // Returns median of all elements so far
+  public double findMedian() {
+    if (list.isEmpty()) { return 0; }
+
+    this.list.sort((a, b) -> Integer.compare(a, b));
+
+    int size = list.size();
+
+    return (size & 1) == 1 
+      ? list.get(size / 2)
+      : ((list.get(size / 2 - 1) + list.get(size / 2)) / 2.0);
+  }
 }
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder obj = new MedianFinder();
- * obj.addNum(num);
- * double param_2 = obj.findMedian();
- */
