1c11b84fb43f32dec4d4487609589f7df9f80231
DragonSenses <kendrickfrancisco@gmail.com>
2025-09-19 17:16:56 -0700

    feat(#295): Add method to locate insertion point

--- LeetCode/295-find-median-from-data-stream/Solution2.java
+++ LeetCode/295-find-median-from-data-stream/Solution2.java
@@ -8,5 +8,16 @@ class MedianFinder {
   public MedianFinder() {
     this.list = new ArrayList<>();
   }
-
+  private int binarySearchInsertIndex(int target) {
+    int left = 0, right = list.size();
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+      if (list.get(mid) < target) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+    return left;
+  }
 }
