f2aecbaa67eb0df08401c4bd8116730fb814192d
DragonSenses <kendrickfrancisco@gmail.com>
2025-09-20 12:25:04 -0700

    feat(#295): Compute median from sorted list

--- LeetCode/295-find-median-from-data-stream/solution2.ts
+++ LeetCode/295-find-median-from-data-stream/solution2.ts
@@ -10,6 +10,17 @@ class MedianFinder {
     this.list.splice(index, 0, num); // Insert at index, shifting elements
   }
 
+  findMedian(): number {
+    const size = this.list.length;
+    if (size === 0) return 0;
+
+    const mid = Math.floor(size / 2);
+
+    return size % 2 === 1
+      ? this.list[mid]
+      : (this.list[mid - 1] + this.list[mid]) / 2;
+  }
+
   private binarySearchInsertIndex(target: number): number {
     let left = 0;
     let right = this.list.length;
