5c246955f537cfc8e82cade49e370cea8da909e3
DragonSenses <kendrickfrancisco@gmail.com>
2025-09-19 17:20:24 -0700

    feat(#295): Insert incoming number at sorted position

--- LeetCode/295-find-median-from-data-stream/Solution2.java
+++ LeetCode/295-find-median-from-data-stream/Solution2.java
@@ -8,6 +8,12 @@ class MedianFinder {
   public MedianFinder() {
     this.list = new ArrayList<>();
   }
+
+  public void addNum(int num) {
+    int index = binarySearchInsertIndex(num);
+    list.add(index, num); // insert at index, shifting elements as needed
+  }
+
   private int binarySearchInsertIndex(int target) {
     int left = 0, right = list.size();
     while (left < right) {
