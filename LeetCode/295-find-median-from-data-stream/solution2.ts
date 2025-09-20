79f30ab11e4b2077f6650c7250de498ff8659115
DragonSenses <kendrickfrancisco@gmail.com>
2025-09-20 12:20:08 -0700

    feat(#295): Use binary search to locate insertion point

--- LeetCode/295-find-median-from-data-stream/solution2.ts
+++ LeetCode/295-find-median-from-data-stream/solution2.ts
@@ -5,4 +5,19 @@ class MedianFinder {
     this.list = [];
   }
 
+  private binarySearchInsertIndex(target: number): number {
+    let left = 0;
+    let right = this.list.length;
+
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+      if (this.list[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+
+    return left;
+  }
 }
