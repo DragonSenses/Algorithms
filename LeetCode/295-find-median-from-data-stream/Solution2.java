6b3458bc0ef1fafe07798c701bfa6d0e60036a33
DragonSenses <kendrickfrancisco@gmail.com>
2025-09-19 17:14:54 -0700

    feat(#295): Create MedianFinder class with list

---
+++ LeetCode/295-find-median-from-data-stream/Solution2.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class MedianFinder {
+  private List<Integer> list;
+
+  /** Initializes the MedianFinder object. */
+  public MedianFinder() {
+    this.list = new ArrayList<>();
+  }
+
+}
