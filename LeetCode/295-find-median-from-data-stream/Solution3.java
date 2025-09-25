3f5bfb4b85c132c85036b4fdd72fac084806d78f
DragonSenses <kendrickfrancisco@gmail.com>
2025-09-25 06:59:16 -0700

    feat(#295): Init MedianFinder w/ min & max heap

---
+++ LeetCode/295-find-median-from-data-stream/Solution3.java
@@ -0,0 +1,13 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class MedianFinder {
+  private PriorityQueue<Integer> lo; // Max-heap
+  private PriorityQueue<Integer> hi; // Min-heap
+
+  public MedianFinder() {
+    lo = new PriorityQueue<>(Collections.reverseOrder());
+    hi = new PriorityQueue<>();
+  }
+
+}
