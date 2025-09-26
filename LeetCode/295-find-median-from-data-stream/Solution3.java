a8c4b0024dfd3a83413c25bae790a6a0e57c2be6
DragonSenses <kendrickfrancisco@gmail.com>
2025-09-26 04:20:54 -0700

    docs(#295): Add comments and Javadoc for clarity

--- LeetCode/295-find-median-from-data-stream/Solution3.java
+++ LeetCode/295-find-median-from-data-stream/Solution3.java
@@ -1,15 +1,27 @@
 import java.util.Collections;
 import java.util.PriorityQueue;
 
+/**
+ * MedianFinder using two heaps to maintain median access.
+ *
+ * - Max-heap (lo) stores the smaller half of the numbers
+ * - Min-heap (hi) stores the larger half of the numbers
+ */
 class MedianFinder {
   private PriorityQueue<Integer> lo; // Max-heap
   private PriorityQueue<Integer> hi; // Min-heap
 
+  /** Initializes the MedianFinder object. */
   public MedianFinder() {
     lo = new PriorityQueue<>(Collections.reverseOrder());
     hi = new PriorityQueue<>();
   }
 
+  /**
+   * Adds a number to the data stream and rebalances the heaps.
+   *
+   * @param num the number to insert
+   */
   public void addNum(int num) {
     lo.offer(num);
     hi.offer(lo.poll());
@@ -19,7 +31,11 @@ class MedianFinder {
     }
   }
 
-
+  /**
+   * Returns the median of all elements added so far.
+   *
+   * @return the median value as a double
+   */
   public double findMedian() {
     if (lo.size() > hi.size()) {
       return lo.peek();
