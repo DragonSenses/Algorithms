e59bc3a3e6fe561d1a4490fef0025328d5600625
DragonSenses <kendrickfrancisco@gmail.com>
2025-10-02 17:49:21 -0700

    feat(#105): Add preorderIndex and index map

--- LeetCode/105-construct-binary-tree-from-preorder-and-inorder-traversal/Solution.java
+++ LeetCode/105-construct-binary-tree-from-preorder-and-inorder-traversal/Solution.java
@@ -1,20 +1,12 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        
-    }
+  private int preorderIndex = 0;
+  private Map<Integer, Integer> inorderIndexMap = new HashMap<>();
+
+  public TreeNode buildTree(int[] preorder, int[] inorder) {
+    return null;
+  }
+
 }
