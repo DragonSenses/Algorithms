c405aa55b8e63dfade1899f5e5efce74157a9483
DragonSenses <kendrickfrancisco@gmail.com>
2025-10-14 19:14:54 -0700

    feat(#621): Init leastInterval and frequency array

--- LeetCode/621-task-scheduler/Solution.java
+++ LeetCode/621-task-scheduler/Solution.java
@@ -1,5 +1,11 @@
 class Solution {
   public int leastInterval(char[] tasks, int n) {
+    // Step 1: Count frequency of each task
+    int[] freq = new int[26];
+    for (char task : tasks) {
+      freq[task - 'A']++;
+    }
 
+    return 0;
   }
 }
