a51800dbfd837e0a62e1491af302794d150dc680
DragonSenses <kendrickfrancisco@gmail.com>
2025-10-19 17:06:22 -0700

    feat(#621): Count frequency of each task

--- LeetCode/621-task-scheduler/solution.ts
+++ LeetCode/621-task-scheduler/solution.ts
@@ -1,3 +1,8 @@
 function leastInterval(tasks: string[], n: number): number {
+  // Step 1: Count frequency of each task
+  const freq: number[] = new Array(26).fill(0);
+  for (const task of tasks) {
+    freq[task.charCodeAt(0) - 'A'.charCodeAt(0)]++;
+  }
 
 }
