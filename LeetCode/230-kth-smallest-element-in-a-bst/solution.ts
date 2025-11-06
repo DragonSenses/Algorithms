b26dc498caee90ea119cba2c28dcbc80aa597b87
DragonSenses <kendrickfrancisco@gmail.com>
2025-11-05 16:46:10 -0800

    feat(#230): Add base case to exit early on null node

--- LeetCode/230-kth-smallest-element-in-a-bst/solution.ts
+++ LeetCode/230-kth-smallest-element-in-a-bst/solution.ts
@@ -22,4 +22,9 @@ class TreeNode {
 function kthSmallest(root: TreeNode | null, k: number): number {
   let count = 0;
   let result = -1;
+
+  function inorder(node: TreeNode | null): void {
+    if (!node) return;
+
+
 }
