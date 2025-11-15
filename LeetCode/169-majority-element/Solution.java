0d19404b62bfa2a530571fb68cc01f7e9a1cd618
DragonSenses <kendrickfrancisco@gmail.com>
2025-11-14 17:59:04 -0800

    feat(#169): Count occurrences of each candidate

--- LeetCode/169-majority-element/Solution.java
+++ LeetCode/169-majority-element/Solution.java
@@ -2,6 +2,12 @@ class Solution {
   public int majorityElement(int[] nums) {
     int n = nums.length;
     for (int i = 0; i < n; i++) {
+      int count = 0;
+      for (int j = 0; j < n; j++) {
+        if (nums[j] == nums[i]) {
+          count++;
+        }
+      }
 
     }
 
