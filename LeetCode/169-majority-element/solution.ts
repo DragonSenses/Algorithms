cd2dcf2d0b80672a160bb13b5282a1be1d6ce31b
DragonSenses <kendrickfrancisco@gmail.com>
2025-11-17 14:26:49 -0800

    feat(#169): Count frequency of each candidate

--- LeetCode/169-majority-element/solution.ts
+++ LeetCode/169-majority-element/solution.ts
@@ -1,6 +1,13 @@
 function majorityElement(nums: number[]): number {
   const n = nums.length;
   for (let i = 0; i < n; i++) {
+    let count = 0;
+    for (let j = 0; j < n; j++) {
+      if (nums[j] === nums[i]) {
+        count++;
+      }
+    }
+
   }
 
 }
