0aee5e90bf4e3bfc685e691764aedea47bdd08a2
DragonSenses <kendrickfrancisco@gmail.com>
2025-11-22 17:47:11 -0800

    feat(#169): Initialize frequency map & threshold

--- LeetCode/169-majority-element/Solution2.java
+++ LeetCode/169-majority-element/Solution2.java
@@ -1,5 +1,10 @@
+import java.util.HashMap;
+
 public class Solution2 {
   public int majorityElement(int[] nums) {
+    HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+    int threshold = nums.length / 2;
+
     return 0;
   }
 }
