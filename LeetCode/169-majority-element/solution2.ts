ee585f65dc19e4400f19e25a8f90f56956bf83f2
DragonSenses <kendrickfrancisco@gmail.com>
2025-11-25 02:45:44 -0800

    feat(#169): Iterate through array & update counts

--- LeetCode/169-majority-element/solution2.ts
+++ LeetCode/169-majority-element/solution2.ts
@@ -2,4 +2,9 @@ function majorityElement(nums: number[]): number {
   const frequencyMap = new Map<number, number>();
   const threshold = Math.floor(nums.length / 2);
 
+  for (const num of nums) {
+    const count = (frequencyMap.get(num) || 0) + 1;
+    frequencyMap.set(num, count);
+  }
+
 }
