c1a53c957a0d777e8a8fc0049b45f9d117d5eafd
DragonSenses <kendrickfrancisco@gmail.com>
2025-12-04 01:45:39 -0800

    feat(#169): Initialize majority var & array length

--- LeetCode/169-majority-element/solution3.ts
+++ LeetCode/169-majority-element/solution3.ts
@@ -3,5 +3,9 @@
  * Reconstructs the majority element bit by bit by counting 1s at each bit position.
  */
 function majorityElement(nums: number[]): number {
+  let majority = 0;
+  const n = nums.length;
+
+
 
 }
