f13cc07aa02b8aa03fd39b5c77f3aa53aad92f53
DragonSenses <kendrickfrancisco@gmail.com>
2025-12-04 01:45:11 -0800

    chore(#169): Add majorityElement method signature

---
+++ LeetCode/169-majority-element/solution3.ts
@@ -0,0 +1,7 @@
+/**
+ * Bitwise Manipulation Approach to find the majority element.
+ * Reconstructs the majority element bit by bit by counting 1s at each bit position.
+ */
+function majorityElement(nums: number[]): number {
+
+}
