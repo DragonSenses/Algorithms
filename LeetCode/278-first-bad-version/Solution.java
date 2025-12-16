3886f8c5218b057c5e65af4c2f3741cc2c63e112
DragonSenses <kendrickfrancisco@gmail.com>
2025-12-16 00:13:41 -0800

    feat(#278): Extend VersionControl class

--- LeetCode/278-first-bad-version/Solution.java
+++ LeetCode/278-first-bad-version/Solution.java
@@ -1,8 +1,5 @@
-/* The isBadVersion API is defined in the parent class VersionControl.
-      boolean isBadVersion(int version); */
-
 public class Solution extends VersionControl {
-    public int firstBadVersion(int n) {
-        
-    }
+  public int firstBadVersion(int n) {
+
+  }
 }
