c143ae2b6d86980d4dc1525270925b9bd094a97c
DragonSenses <kendrickfrancisco@gmail.com>
2025-12-16 00:21:04 -0800

    feat(#278): Add loop and compute midpoint safely

--- LeetCode/278-first-bad-version/Solution.java
+++ LeetCode/278-first-bad-version/Solution.java
@@ -3,5 +3,11 @@ public class Solution extends VersionControl {
     int left = 1;
     int right = n;
 
+    while (left < right) {
+      // Safe midpoint calculation to avoid overflow
+      int mid = left + (right - left) / 2;
+
+    }
+
   }
 }
