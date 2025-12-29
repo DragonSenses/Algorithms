d4680b0922542b055155ccb9b5b16d9118f237a6
DragonSenses <kendrickfrancisco@gmail.com>
2025-12-29 01:31:00 -0800

    feat(#199): Initialize rightside result list

--- LeetCode/199-binary-tree-right-side-view/Solution.java
+++ LeetCode/199-binary-tree-right-side-view/Solution.java
@@ -1,12 +1,12 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 class Solution {
   public List<Integer> rightSideView(TreeNode root) {
-
-    if (root == null) return null;
-
+    List<Integer> rightside = new ArrayList<>();
+    if (root == null) return rightside;
 
   }
 }
