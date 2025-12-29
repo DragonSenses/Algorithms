5d9f0e7225eb31a29810f79886c003cf9fed0c30
DragonSenses <kendrickfrancisco@gmail.com>
2025-12-29 00:48:29 -0800

    chore(#199): Remove redundant comment; import List

--- LeetCode/199-binary-tree-right-side-view/Solution.java
+++ LeetCode/199-binary-tree-right-side-view/Solution.java
@@ -1,20 +1,6 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+import java.util.List;
+
 class Solution {
   public List<Integer> rightSideView(TreeNode root) {
-      
   }
 }
