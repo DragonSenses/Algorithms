543086a98c5505be2302e9a9e8ea4d4e6b901bde
DragonSenses <kendrickfrancisco@gmail.com>
2026-01-12 08:07:05 -0800

    feat(#199): Add imports and initialize result list

--- LeetCode/199-binary-tree-right-side-view/Solution2.java
+++ LeetCode/199-binary-tree-right-side-view/Solution2.java
@@ -1,20 +1,10 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution2 {
   public List<Integer> rightSideView(TreeNode root) {
-      
+    List<Integer> rightside = new ArrayList<>();
+    return rightside;
   }
+
 }
