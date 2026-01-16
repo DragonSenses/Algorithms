86c6c8550007d2e4f17900050c840ff57f1b0076
DragonSenses <kendrickfrancisco@gmail.com>
2026-01-16 11:26:10 -0800

    feat(#199): Add TreeNode class for binary tree

--- LeetCode/199-binary-tree-right-side-view/solution2.ts
+++ LeetCode/199-binary-tree-right-side-view/solution2.ts
@@ -1,16 +1,16 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+class TreeNode {
+  val: number;
+
+  left: TreeNode | null;
+
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
 
 function rightSideView(root: TreeNode | null): number[] {
     
