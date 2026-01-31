a1bf050cc0f1c7c7ab88213afd50d0e134998208
DragonSenses <kendrickfrancisco@gmail.com>
2026-01-30 17:16:44 -0800

    chore(#297): Remove redundant comments

--- LeetCode/297-serialize-and-deserialize-binary-tree/Codec.java
+++ LeetCode/297-serialize-and-deserialize-binary-tree/Codec.java
@@ -1,26 +1,12 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 public class Codec {
 
-    // Encodes a tree to a single string.
-    public String serialize(TreeNode root) {
-        
-    }
+  // Encodes a tree to a single string.
+  public String serialize(TreeNode root) {
 
-    // Decodes your encoded data to tree.
-    public TreeNode deserialize(String data) {
-        
-    }
+  }
+
+  // Decodes your encoded data to tree.
+  public TreeNode deserialize(String data) {
+
+  }
 }
-
-// Your Codec object will be instantiated and called as such:
-// Codec ser = new Codec();
-// Codec deser = new Codec();
-// TreeNode ans = deser.deserialize(ser.serialize(root));
