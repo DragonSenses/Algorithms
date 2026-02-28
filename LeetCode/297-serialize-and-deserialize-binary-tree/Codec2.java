6c05091022ed7ecb84b530676181ed3d570f30eb
DragonSenses <kendrickfrancisco@gmail.com>
2026-02-28 11:57:28 -0800

    feat(#297): Add null marker and delimiter

--- LeetCode/297-serialize-and-deserialize-binary-tree/Codec2.java
+++ LeetCode/297-serialize-and-deserialize-binary-tree/Codec2.java
@@ -1,5 +1,8 @@
 public class Codec2 {
 
+  private static final String NULL_MARKER = "#";
+  private static final String DELIM = ",";
+
   // Encodes a tree to a single string.
   public String serialize(TreeNode root) {
 
