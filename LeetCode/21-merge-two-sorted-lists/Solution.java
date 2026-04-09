342e04179272671a5c5ee0a4a028ad43ece7b53d
DragonSenses <kendrickfrancisco@gmail.com>
2026-04-09 11:51:13 -0700

    feat(#21): Handle base cases for empty lists

--- LeetCode/21-merge-two-sorted-lists/Solution.java
+++ LeetCode/21-merge-two-sorted-lists/Solution.java
@@ -1,5 +1,12 @@
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        
-    }
+  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+   if (list1 == null) {
+    return list2;
+   }
+
+   if (list2 == null) {
+    return list1;
+   }
+   
+  }
 }
