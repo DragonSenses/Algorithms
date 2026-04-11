a40fb348ce92f67e50d7aefccbad9ddfe36ae8b3
DragonSenses <kendrickfrancisco@gmail.com>
2026-04-11 12:07:49 -0700

    feat(#21): Recurse on the remainder

--- LeetCode/21-merge-two-sorted-lists/Solution.java
+++ LeetCode/21-merge-two-sorted-lists/Solution.java
@@ -9,7 +9,11 @@ class Solution {
    }
 
    if (list1.val <= list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2);
+    return list1;
    } else {
+    list2.next = mergeTwoLists(list1, list2.next);
+    return list2;
    }
   }
 }
