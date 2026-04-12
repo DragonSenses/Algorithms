a8924b569d8f39177c4c631d17c55e71219577ba
DragonSenses <kendrickfrancisco@gmail.com>
2026-04-12 16:14:40 -0700

    docs(#21): Add comments and Javadoc for clarity

--- LeetCode/21-merge-two-sorted-lists/Solution.java
+++ LeetCode/21-merge-two-sorted-lists/Solution.java
@@ -1,19 +1,35 @@
+/**
+ * Merges two sorted singly linked lists into one sorted list.
+ *
+ * This method performs a recursive merge:
+ * - If either list is empty, the other list is returned immediately.
+ * - Otherwise, the node with the smaller value becomes the head of the merged list.
+ * - The chosen node's next pointer is set to the result of merging the remaining nodes.
+ *
+ * The merge is done by reusing existing nodes; no new nodes are allocated.
+ *
+ * @param list1 the head of the first sorted linked list
+ * @param list2 the head of the second sorted linked list
+ * @return the head of the merged sorted linked list
+ */
 class Solution {
   public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-   if (list1 == null) {
-    return list2;
-   }
 
-   if (list2 == null) {
-    return list1;
-   }
+    // Base case: if one list is empty, return the other
+    if (list1 == null) {
+      return list2;
+    }
+    if (list2 == null) {
+      return list1;
+    }
 
-   if (list1.val <= list2.val) {
-    list1.next = mergeTwoLists(list1.next, list2);
-    return list1;
-   } else {
-    list2.next = mergeTwoLists(list1, list2.next);
-    return list2;
-   }
+    // Choose the smaller node and recursively merge the remainder
+    if (list1.val <= list2.val) {
+      list1.next = mergeTwoLists(list1.next, list2); // attach merged remainder
+      return list1; // list1 is the new head
+    } else {
+      list2.next = mergeTwoLists(list1, list2.next); // attach merged remainder
+      return list2; // list2 is the new head
+    }
   }
 }
