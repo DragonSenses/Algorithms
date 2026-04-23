e863bff4800054b05d1bd205ea7f9ac345f44d52
DragonSenses <kendrickfrancisco@gmail.com>
2026-04-23 01:31:24 -0700

    feat(#21): Compare node values in traversal

--- LeetCode/21-merge-two-sorted-lists/Solution2.java
+++ LeetCode/21-merge-two-sorted-lists/Solution2.java
@@ -4,6 +4,9 @@ class Solution2 {
     ListNode current = sentinel;
 
     while ((list1 != null) && (list2 != null)) {
+      if (list1.val <= list2.val) {
+        current.next = list1;
+      } 
 
   } 
 }
