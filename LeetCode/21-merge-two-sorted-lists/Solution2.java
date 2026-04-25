fdded086f7ec05aa927c4488951cbf1699e5cbf1
DragonSenses <kendrickfrancisco@gmail.com>
2026-04-25 01:33:51 -0700

    feat(#21): Add list1 and list2 merge cases

--- LeetCode/21-merge-two-sorted-lists/Solution2.java
+++ LeetCode/21-merge-two-sorted-lists/Solution2.java
@@ -7,7 +7,10 @@ class Solution2 {
       if (list1.val <= list2.val) {
         current.next = list1;
         list1 = list1.next;
-      } 
+      } else {
+        current.next = list2;
+        list2 = list2.next;
+      }
 
   } 
 }
