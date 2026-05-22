ba16552e5e7c9d6948ded03b83e529b40eae5da9
DragonSenses <kendrickfrancisco@gmail.com>
2026-05-22 04:38:06 -0700

    feat(#141): Detect revisited node using set

--- LeetCode/141-linked-list-cycle/Solution.java
+++ LeetCode/141-linked-list-cycle/Solution.java
@@ -5,6 +5,9 @@ public class Solution {
   public boolean hasCycle(ListNode head) {
     Set<ListNode> visited = new HashSet<>();
     while (head != null) {
+      if (visited.contains(head)) {
+        return true;
+      }
 
     }
   }
