107653522ac5895691b3da844201ec84bdea1608
DragonSenses <kendrickfrancisco@gmail.com>
2026-06-15 11:15:02 -0700

    feat(#141): Check when slow and fast pointers meet

--- LeetCode/141-linked-list-cycle/Solution2.java
+++ LeetCode/141-linked-list-cycle/Solution2.java
@@ -10,6 +10,10 @@ public class Solution2 {
     while (fast != null && fast.next != null) {
       slow = slow.next;
       fast = fast.next.next;
+
+      if (slow == fast) {
+        return true;
+      }
     }
   }
 }
