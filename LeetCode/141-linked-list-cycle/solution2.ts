8ad6b09dedec09e6fbc95080829538f1a853f8ba
DragonSenses <kendrickfrancisco@gmail.com>
2026-06-17 11:13:06 -0700

    feat(#141): Add ListNode class for linked list

--- LeetCode/141-linked-list-cycle/solution2.ts
+++ LeetCode/141-linked-list-cycle/solution2.ts
@@ -1,3 +1,12 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
 function hasCycle(head: ListNode | null): boolean {
 
 };
