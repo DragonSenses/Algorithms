8e6f068e94f33ec58414970a859821a001264c8e
DragonSenses <kendrickfrancisco@gmail.com>
2026-09-25 03:20:39 -0700

    feat(#438): Add early return

--- LeetCode/438-find-all-anagrams-in-a-string/Solution.java
+++ LeetCode/438-find-all-anagrams-in-a-string/Solution.java
@@ -1,6 +1,8 @@
 class Solution {
   public List<Integer> findAnagrams(String s, String p) {
     List<Integer> result = new ArrayList<>();
+    if (s.length() < p.length())
+      return result;
 
   }
 }
